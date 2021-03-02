2f0932ef3945fb13201d4f24756c67853e7a3cfd
Chris Lyons <chris@lyonsgroup.family>
2021-03-02 18:15:09 -0500

    Initial commit/migration

--- compute.sh
+++ compute.sh
@@ -31,5 +31,12 @@ pip3 install --upgrade pip
 cd /etc/init.d
 ./vmware-tools restart
 
+#update ip of network card 3 so that it is static in the 192.168.0.XXX range
+IP=(`awk -F'=' '$1 == "IPADDR" {print $2}' /etc/sysconfig/network-scripts/ifcfg-eth1`)
+octet=`echo "$IP" | cut -d . -f 4`
+cn=`echo $octet + 35 | bc`
+sed -i "s/IPADDR=$IP/IPADDR=192.168.0.$cn/g" /etc/sysconfig/network-scripts/ifcfg-eth1
+ifdown eth1 && ifup eth1
+
 #remove so as to not run again
 rm -rf /etc/rc.d/rc.local
