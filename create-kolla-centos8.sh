56d28115d3265f65d4ef3fddb41d85ebc6f914cc
Chris Lyons <chris@lyonsgroup.family>
2021-03-14 08:29:37 -0400

    Initial commit/migration

--- create-kolla-centos8.sh
+++ create-kolla-centos8.sh
@@ -25,8 +25,8 @@ chmod 777 /tmp/storage_hosts
 
 #### setup static network local DNS entries
 echo "echo '192.168.0.252 app-external.lyonsgroup.family' >> /etc/hosts;" >> /tmp/dns_hosts
-echo "echo '11.0.0.254 app-internal.lyonsgroup.family' >> /etc/hosts;" >> /tmp/dns_hosts
-echo "echo '11.0.0.60 cloudsupport.lyonsgroup.family' >> /etc/hosts;" >> /tmp/dns_hosts
+echo "echo '10.0.0.254 app-internal.lyonsgroup.family' >> /etc/hosts;" >> /tmp/dns_hosts
+echo "echo '10.0.0.60 cloudsupport.lyonsgroup.family' >> /etc/hosts;" >> /tmp/dns_hosts
 ####
 ######### Openstack VM types
 
