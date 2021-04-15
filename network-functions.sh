1635e2e7f28818c435887630695d645487962e74
Chris Lyons <chris@lyonsgroup.family>
2021-04-15 13:04:00 -0400

    Initial commit/migration

--- network-functions.sh
+++ network-functions.sh
@@ -80,11 +80,6 @@ function networkInformation {
           addresses+=($ip_addr)
         fi
 
-        # If storage address, add to array to build rings later
-        if [[ "$vm_type" == "storage" ]]; then
-          echo "$ip_addr" >> /tmp/storage_hosts
-        fi
-
         if [[ $DEFAULT_ROUTE == "Internal" ]]; then
           if [[ $default_flag == "0" ]]; then
             default_set=""
