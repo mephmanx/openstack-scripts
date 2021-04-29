ea9eb8169230974d386e273e85d429a462d9c812
Chris Lyons <chris@lyonsgroup.family>
2021-04-29 09:29:55 -0400

    Initial commit/migration

--- cloudsupport.sh
+++ cloudsupport.sh
@@ -6,20 +6,21 @@ set -x                             # tell sh to display commands before executio
 yum clean all && yum update -y  #this is only to make the next call work, DONT remove!
 pip3 install --upgrade pip
 
-yum-config-manager \
---add-repo \
-https://download.docker.com/linux/centos/docker-ce.repo
-
-yum install -y docker-ce docker-ce-cli containerd.io --nobest
-
-systemctl start docker
-systemctl enable docker
-chkconfig docker on
-
 working_dir=`pwd`
 chmod 777 /tmp/openstack-env.sh
 source ./tmp/openstack-env.sh
 cd $working_dir
+
+runuser -l root -c  "yum install -y https://$GITHUB_TOKEN@raw.githubusercontent.com/mephmanx/cloud-libs/master/containerd.io-1.2.6-3.3.el7.x86_64.rpm"
+sleep 5
+runuser -l root -c  "yum install -y https://$GITHUB_TOKEN@raw.githubusercontent.com/mephmanx/cloud-libs/master/docker-ce-cli-18.09.9-3.el7.x86_64.rpm"
+sleep 5
+runuser -l root -c  "yum install -y https://$GITHUB_TOKEN@raw.githubusercontent.com/mephmanx/cloud-libs/master/docker-ce-18.09.9-3.el7.x86_64.rpm"
+sleep 5
+
+systemctl start docker
+systemctl enable docker
+chkconfig docker on
 
 systemctl restart docker
 docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PWD
@@ -29,61 +30,17 @@ curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compo
 chmod +x /usr/local/bin/docker-compose
 
 cd /root
-git clone https://github.com/mephmanx/Portus.git
+yum install -y tar
 
-wget -O /root/Portus/docker-compose.yml -d --header="Authorization: Bearer $GITHUB_TOKEN" https://raw.githubusercontent.com/mephmanx/openstack-scripts/master/portus-compose.yml
+wget -O /tmp/harbor.tgz https://github.com/goharbor/harbor/releases/download/v2.1.5/harbor-offline-installer-v2.1.5.tgz
+tar xzvf /tmp/harbor.tgz
 
-cd /root/Portus
-sed -i "s/password=portus/password=${PORTUS_PASSWORD}/g" /root/Portus/examples/compose/clair/clair.yml
+wget -O /root/harbor/harbor.yml -d --header="Authorization: Bearer $GITHUB_TOKEN" https://raw.githubusercontent.com/mephmanx/openstack-scripts/master/harbor.yml
+sed -i "s/{MACHINE_FQDN}/${MACHINE_FQDN}/g" /root/harbor/harbor.yml
+sed -i "s/{PORTUS_PASSWORD}/${PORTUS_PASSWORD}/g" /root/harbor/harbor.yml
+sed -i "s/{DATABASE_PASSWORD}/${DATABASE_PASSWORD}/g" /root/harbor/harbor.yml
 
-docker-compose up -d
-sleep 90
-##### Set up cloud accounts
-jq --version || apt-get install -y jq || yum install -y jq || zypper install -y jq
-
-export PORTUS_HOST=$MACHINE_FQDN:$MACHINE_PORT
-# -- bootstrap (no one has opened the web ui and auto-registered as first super admin)
-# first, we need the secret (the token) to authenticate to Portus API
-# - The [/bootstrap] Endpoints, POST, to crate users, and PUT to modify permissions
-# http://port.us.org/docs/API.html#spec-method-post-api-v1-users-bootstrap
-
-# => With this first Portus API call, you retrieve in the response the token you are going to use to
-#     authenticate to the Portus REST API in all the next Portus API calls below
-export FIRST_SUPER_ADMIN_USERNAME=$PORTUS_USERNAME
-export FIRST_SUPER_ADMIN_PASSWORD=$PORTUS_PASSWORD
-export FIRST_SUPER_ADMIN_EMAIL=$ADMIN_EMAIL
-# We don't need the role, cause bootstraping implicitly means
-# giving admin role to bootstraped first user.
-# export FIRST_SUPER_ADMIN_ROLE=admin
-
-export PAYLOAD="{\"user\": {\"username\": \"$FIRST_SUPER_ADMIN_USERNAME\", \"email\": \"$FIRST_SUPER_ADMIN_EMAIL\", \"password\": \"$FIRST_SUPER_ADMIN_PASSWORD\", \"display_name\": \"$FIRST_SUPER_ADMIN_EMAIL\"}}"
-
-curl -H 'Content-type: application/json' -H 'Accept: application/json' -X POST --data "$PAYLOAD" https://$PORTUS_HOST/api/v1/users/bootstrap > /tmp/portus.bootstrap.json
-
-export  PORTUS_API_BOOT_TOKEN=$(cat /tmp/portus.bootstrap.json | jq '.plain_token' | awk -F '"' '{print $2}')
-# And this one guy , $PORTUS_API_BOOT_TOKEN , it's your most critical portus secret, so you immediately secrue it into a secret manager like ansible secret manager, kubernetes / openshift secret manager, personally I prefer hashicorp vault.
-# Then you'll use a credential helper, configured with your secret manager, to resolve your Portus API auth secret, for all subsequent Portus API calls.
-# Or best: you revoke it once bootstrap process completed
-#
-# Now we can authenticate to portus api using the token :
-# --- #
-curl -X GET --header 'Accept: application/json' --header "Portus-Auth: $FIRST_SUPER_ADMIN_USERNAME:$PORTUS_API_BOOT_TOKEN" https://${PORTUS_HOST}/api/v1/_ping | jq '.'
-curl -X GET --header 'Accept: application/json' --header "Portus-Auth: $FIRST_SUPER_ADMIN_USERNAME:$PORTUS_API_BOOT_TOKEN" https://${PORTUS_HOST}/api/v1/health | jq '.'
-curl -X GET --header 'Accept: application/json' --header "Portus-Auth: $FIRST_SUPER_ADMIN_USERNAME:$PORTUS_API_BOOT_TOKEN" https://${PORTUS_HOST}/api/v1/users | jq '.[]'
-
-# --->> REGISTRY CONFIG
-# ok, so now let's configure the OCI image regsitry in portus :
-#
-# 1./ Configuring the registry, so portus knows how to reach it, onthe network.
-# [POST /api/v1/registries]
-export PORTUS_REGISTRY_NAME=$FIRST_SUPER_ADMIN_USERNAME
-export PORTUS_REGISTRY_HOSTNAME=$MACHINE_FQDN:$REGISTRY_PORT
-export PORTUS_REGISTRY_USE_SSL=true
-export PORTUS_REGISTRY_EXTERNAL_HOSTNAME=$MACHINE_FQDN
-export PAYLOAD="{\"registry\": {\"name\": \"$PORTUS_REGISTRY_NAME\", \"hostname\": \"$PORTUS_REGISTRY_HOSTNAME\", \"use_ssl\": \"$PORTUS_REGISTRY_USE_SSL\", \"external_hostname\": \"$PORTUS_REGISTRY_EXTERNAL_HOSTNAME\"}}"
-
-curl --header 'Content-type: application/json' --header 'Accept: application/json' --header "Portus-Auth: $FIRST_SUPER_ADMIN_USERNAME:$PORTUS_API_BOOT_TOKEN" -X POST --data "$PAYLOAD" https://${PORTUS_HOST}/api/v1/registries | jq '.'
-
+./install.sh --with-notary --with-trivy --with-chartmuseum
 
 #remove so as to not run again
 rm -rf /etc/rc.d/rc.local
