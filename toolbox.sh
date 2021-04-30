20cdd87b893804255f9859bdd3df233115c8927e
Chris Lyons <chris@lyonsgroup.family>
2021-04-30 15:06:36 -0400

    Initial commit/migration

--- toolbox.sh
+++ toolbox.sh
@@ -12,7 +12,7 @@ cp -r /root/openstack-scripts/*.sh /root/openstack-setup;
 cp /root/openstack-scripts/*.cfg /root/openstack-setup;
 
 cd /root/openstack-setup
-/root/openstack-setup/create-$1-centos8.sh 192.168.3.100 $2;
+/root/openstack-setup/$2/create-$2-centos8.sh 192.168.3.100 $3;
 
 END)
 
