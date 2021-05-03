09b80b0b6b0e50605dec3746907d885e3f82f18a
Chris Lyons <chris@lyonsgroup.family>
2021-05-03 15:05:14 -0400

    Initial commit/migration

--- exec.sh
+++ exec.sh
@@ -13,7 +13,7 @@ mkdir /root/openstack-scripts/certs
 
 cp -r /root/openstack-setup/certs/*.* /root/openstack-scripts/certs;
 
-"$1" | ./buildEnvironment.sh --$2 --$3 --$4 -o 192.168.3.100 -p
+"$1" | ./root/openstack-scripts/buildEnvironment.sh --$2 --$3 --$4 -o 192.168.3.100 -p
 END)
 
 ssh -l root 192.168.3.101 "$CMD"
