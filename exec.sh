59ee06bf6858df52a83e1abcfd08fddb447018f3
Chris Lyons <chris@lyonsgroup.family>
2021-05-03 15:55:09 -0400

    Initial commit/migration

--- exec.sh
+++ exec.sh
@@ -1,6 +1,6 @@
 #can ONLY be run as root!  sudo to root
 source ./openstack-setup/openstack-env.sh
-echo "$1" "$2" "$3" "$4"
+
 CMD=$(cat <<END
 
 rm -rf /root/openstack-setup;
@@ -17,5 +17,5 @@ cd /root/openstack-scripts
 
 echo "$1" | ./buildEnvironment.sh --"$2" --"$3" --"$4" -o 192.168.3.100 -p
 END)
-
+echo "$CMD"
 ssh -l root 192.168.3.101 "$CMD"
