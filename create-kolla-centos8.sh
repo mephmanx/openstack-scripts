df8a0204885423e37c6566a2b80f24c8f39f075c
Chris Lyons <chris@lyonsgroup.family>
2021-05-09 15:29:11 -0400

    adding auth

--- create-kolla-centos8.sh
+++ create-kolla-centos8.sh
@@ -38,9 +38,6 @@ INTERNAL_VIP_DNS="$APP_INTERNAL_HOSTNAME.$DOMAIN_NAME"
 
 EXTERNAL_VIP="192.168.1.252"
 EXTERNAL_VIP_DNS="$APP_EXTERNAL_HOSTNAME.$DOMAIN_NAME"
-
-SUPPORT_HOST="10.0.20.200"
-
 ##############################################
 
 #### setup static network local DNS entries
@@ -48,7 +45,6 @@ echo "export EXTERNAL_VIP=$EXTERNAL_VIP" >> /tmp/global_addresses.sh
 echo "export INTERNAL_VIP=$INTERNAL_VIP" >> /tmp/global_addresses.sh
 echo "export EXTERNAL_VIP_DNS=$EXTERNAL_VIP_DNS" >> /tmp/global_addresses.sh
 echo "export INTERNAL_VIP_DNS=$INTERNAL_VIP_DNS" >> /tmp/global_addresses.sh
-echo "export SUPPORT_HOST=$SUPPORT_HOST" >> /tmp/global_addresses.sh
 #########################
 
 ######### Openstack VM types
