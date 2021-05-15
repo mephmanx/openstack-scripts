35197f8393c509be9d97cff453a390ead6e77da4
Chris Lyons <chris@lyonsgroup.family>
2021-05-14 23:23:17 -0400

    adding auth

--- cloudsupport.sh
+++ cloudsupport.sh
@@ -11,12 +11,6 @@ chmod 777 /tmp/openstack-env.sh
 source ./tmp/openstack-env.sh
 cd $working_dir
 
-#runuser -l root -c  "yum install -y https://mephmanx:$GITHUB_TOKEN@raw.githubusercontent.com/mephmanx/cloud-libs/master/containerd.io-1.2.6-3.3.el7.x86_64.rpm"
-#sleep 5
-#runuser -l root -c  "yum install -y https://mephmanx:$GITHUB_TOKEN@raw.githubusercontent.com/mephmanx/cloud-libs/master/docker-ce-cli-18.09.9-3.el7.x86_64.rpm"
-#sleep 5
-#runuser -l root -c  "yum install -y https://mephmanx:$GITHUB_TOKEN@raw.githubusercontent.com/mephmanx/cloud-libs/master/docker-ce-18.09.9-3.el7.x86_64.rpm"
-#sleep 5
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce docker-ce-cli containerd.io
 
