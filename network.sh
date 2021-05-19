f5d0e2477da1f9788cf1b9d334c6d6bd95a18a83
Chris Lyons <chris@lyonsgroup.family>
2021-05-19 16:22:23 -0400

    adding auth

--- network.sh
+++ network.sh
@@ -10,7 +10,7 @@ common_second_boot_setup
 
 ######## Put type specific code
 
-runuser -l root -c  '/sbin/ip link set eth2 promisc on'
+#runuser -l root -c  '/sbin/ip link set eth2 promisc on'
 #runuser -l root -c  '/sbin/ip link set eth0 promisc on'
 
 sed '/^IPADDR/d' -i /tmp/eth2
@@ -23,14 +23,14 @@ runuser -l root -c  "cat /tmp/eth2 > /etc/sysconfig/network-scripts/ifcfg-eth2"
 ############################
 
 #remove so as to not run again
-rm -rf /etc/rc.d/rc.local
-
-cat > /etc/rc.d/rc.local <<EOF
-#!/bin/bash
-
-/sbin/ip link set eth2 promisc on
-EOF
-
-chmod a+x /etc/rc.d/rc.local
+#rm -rf /etc/rc.d/rc.local
+#
+#cat > /etc/rc.d/rc.local <<EOF
+##!/bin/bash
+#
+#/sbin/ip link set eth2 promisc on
+#EOF
+#
+#chmod a+x /etc/rc.d/rc.local
 
 reboot
