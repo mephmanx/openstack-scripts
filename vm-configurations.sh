b20ac00f210e37fa4e5f0c6fe3e5a83d453f65a5
Chris Lyons <chris@lyonsgroup.family>
2021-05-24 18:03:45 -0400

    adding openstack server

--- vm-configurations.sh
+++ vm-configurations.sh
@@ -133,7 +133,8 @@ function create_vm_kvm {
 
   #########################
   printf -v virt_disk_string '%s ' "${virt_disk_list[@]}"
-  echo "virt-install --virt-type kvm --name $2 --memory ${memory_ct}00 --vcpus $cpu_ct $virt_disk_string --cdrom /var/tmp/$2-iso.iso --os-variant centos8 --graphics vnc"
+  tr -dc '[[:print:]]' <<< "$virt_disk_string"
+  echo "virt-install --virt-type kvm --name $2 --memory ${memory_ct}00 --vcpus $cpu_ct $virt_disk_string--cdrom /var/tmp/$2-iso.iso --os-variant centos8 --graphics vnc"
   virt-install --virt-type kvm --name $2 \
     --memory ${memory_ct}00 \
     --vcpus $cpu_ct \
