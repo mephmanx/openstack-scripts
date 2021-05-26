f5d88cef6059b4e2e408289534ea67f61e34a717
chris <chris@lyonsgroup.family>
2021-05-26 15:11:33 -0400

    adding promisc

--- create-cloudsupport-centos8.sh
+++ create-cloudsupport-centos8.sh
@@ -30,7 +30,7 @@ closeOutAndBuildKickstartAndISO "${kickstart_file}" "cloudsupport"
 esxi-scp -H $HOSTNAME -n /var/tmp/cloudsupport-iso.iso -l /vmfs/volumes/$ISO_DISK_NAME/isos
 
 esxi-vm-create -n cloudsupport --summary --iso /vmfs/volumes/$ISO_DISK_NAME/isos/cloudsupport-iso.iso \
-  -c 2 -m 16 -S HP-Disk -v HP-Disk:400 -N Openstack-Internal-Static,Openstack-Local-Static -V --summary \
+  -c 2 -m 16 -S HP-Disk -v HP-Disk:400 -N os-int,os-loc -V --summary \
   -o 'cpuid.coresPerSocket = "1",
               vhv.enable = "TRUE",
               vvtd.enable = "TRUE",
