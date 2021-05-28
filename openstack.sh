749089eba7e73917daf59a75d70e32b54c55d0c2
Chris Lyons <chris@lyonsgroup.family>
2021-05-28 13:46:12 -0400

    adding openstack server

--- openstack.sh
+++ openstack.sh
@@ -43,23 +43,23 @@ nmcli connection down bond0 && nmcli connection up bond0
 ##########################################
 
 ################# Add bridge
-ip link add br0 type bridge
-ip address add dev br0 192.168.1.200/24
-ip link set bond0 master br0
-
-cat > /etc/sysctl.d/99-netfilter-bridge.conf <<EOF
-net.bridge.bridge-nf-call-ip6tables = 0
-net.bridge.bridge-nf-call-iptables = 0
-net.bridge.bridge-nf-call-arptables = 0
-EOF
-
-modprobe br_netfilter
-
-cat > /etc/modules-load.d/br_netfilter.conf <<EOF
-br_netfilter
-EOF
-
-sysctl -p /etc/sysctl.d/99-netfilter-bridge.conf
+#ip link add br0 type bridge
+#ip address add dev br0 192.168.1.200/24
+#ip link set bond0 master br0
+#
+#cat > /etc/sysctl.d/99-netfilter-bridge.conf <<EOF
+#net.bridge.bridge-nf-call-ip6tables = 0
+#net.bridge.bridge-nf-call-iptables = 0
+#net.bridge.bridge-nf-call-arptables = 0
+#EOF
+#
+#modprobe br_netfilter
+#
+#cat > /etc/modules-load.d/br_netfilter.conf <<EOF
+#br_netfilter
+#EOF
+#
+#sysctl -p /etc/sysctl.d/99-netfilter-bridge.conf
 ###########################
 
 ################# setup KVM and kick off openstack cloud create
