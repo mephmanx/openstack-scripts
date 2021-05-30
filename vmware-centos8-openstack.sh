30b6763495126f3746f65a2dcf4efd7fc9ba081f
Chris Lyons <chris@lyonsgroup.family>
2021-05-30 09:10:09 -0400

    adding openstack server

--- vmware-centos8-openstack.sh
+++ vmware-centos8-openstack.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-# chkconfig: 2345 20 80
-# description: Description comes here....
-# ALWAYS have content in functions otherwise you get syntax errors!
 
 # Source function library.
-. /etc/init.d/functions
 . /tmp/vm_functions.sh
 
-start() {
-# code to start app comes here
-# example: daemon program_name &
+
 exec 1>/tmp/start-install.log 2>&1 # send stdout and stderr from rc.local to a log file
 set -x                             # tell sh to display commands before execution
 
@@ -32,36 +26,5 @@ chmod +x /etc/rc.d/rc.local
 
 ########################
 #remove this script so it only runs once on machine start
-rm -rf /etc/init.d/vmware-centos8.sh
+rm -rf /etc/rc.d/rc.local
 reboot
-
-}
-
-stop() {
-    # code to stop app comes here 
-    # example: killproc program_name
-    /bin/true
-}
-
-case "$1" in 
-    start)
-       start
-       ;;
-    stop)
-        /bin/true
-       stop
-       ;;
-    restart)
-       stop
-       start
-       ;;
-    status)
-        /bin/true
-       # code to check status of app comes here 
-       # example: status program_name
-       ;;
-    *)
-       echo "Usage: $0 {start|stop|status|restart}"
-esac
-
-exit 0 
