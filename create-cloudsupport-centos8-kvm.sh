72f5c6f62aa164d0814bd6912023b08d656a629a
Chris Lyons <chris@lyonsgroup.family>
2021-05-30 19:29:48 -0400

    adding openstack server

---
+++ create-cloudsupport-centos8-kvm.sh
@@ -0,0 +1,30 @@
+source ./iso-functions.sh
+source ./openstack-env.sh
+
+
+virsh destroy "cloudsupport"
+virsh undefine "cloudsupport"
+
+IFS=
+kickstart_file=centos-8-kickstart-cloudsupport.cfg
+####initial certs###############
+letsEncryptAndCockpitCerts ${kickstart_file}
+###############################
+
+########### add passwords in
+sed -i 's/{GITHUB_TOKEN}/'$GITHUB_TOKEN'/g' ${kickstart_file}
+sed -i 's/{CENTOS_ROOT_PWD}/'$CENTOS_ROOT_PWD'/g' ${kickstart_file}
+###########################
+
+############### Secrets file ################
+echo 'cat > /tmp/openstack-env.sh <<EOF' >> ${kickstart_file}
+cat ./openstack-env.sh >> ${kickstart_file}
+echo 'EOF' >> ${kickstart_file}
+###############################
+
+closeOutAndBuildKickstartAndISO "${kickstart_file}" "cloudsupport"
+
+echo "virt-install --virt-type kvm --name cloudsupport --memory 1600 --vcpus 2 --disk pool=HP-Disk,size=400,bus=scsi --cdrom /var/tmp/cloudsupport-iso.iso --network bridge=os-int-static,model=rtl8139 --network bridge=os-loc-static,model=rtl8139 --os-variant centos8 --graphics vnc"
+
+eval "virt-install --virt-type kvm --name cloudsupport --memory 1600 --vcpus 2 --disk pool=HP-Disk,size=400,bus=scsi --cdrom /var/tmp/cloudsupport-iso.iso --network bridge=os-int-static,model=rtl8139 --network bridge=os-loc-static,model=rtl8139 --os-variant centos8 --graphics vnc" &
+
