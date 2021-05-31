9390a8efeab4d7e95b8f8a304104d5665802393d
Chris Lyons <chris@lyonsgroup.family>
2021-05-31 11:26:36 -0400

    adding openstack server

--- openstack.sh
+++ openstack.sh
@@ -87,7 +87,7 @@ virsh pool-autostart HP-SSD
 virsh pool-start HP-SSD
 
 ## HP-SSD pool
-virsh pool-define-as HP-EXT logical - - - - "/HP-EXT"
+virsh pool-define-as HP-EXT dir - - - - "/HP-EXT"
 virsh pool-build HP-EXT
 virsh pool-autostart HP-EXT
 virsh pool-start HP-EXT
