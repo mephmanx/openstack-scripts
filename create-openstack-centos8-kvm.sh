66ad755e9a08883c46b9345d1f1e017816095ebb
Chris Lyons <chris@lyonsgroup.family>
2021-05-30 22:44:09 -0400

    adding openstack server

---
+++ create-openstack-centos8-kvm.sh
@@ -0,0 +1,40 @@
+source ./iso-functions.sh
+source ./openstack-env.sh
+
+cd /tmp/openstack-scripts;
+git pull;
+cd /tmp/openstack-setup;
+git pull;
+
+rm -rf /var/tmp/*.*;
+
+cp /tmp/openstack-scripts/*.sh /tmp/openstack-setup;
+cp /tmp/openstack-scripts/*.cfg /tmp/openstack-setup;
+
+virsh destroy "openstack"
+virsh undefine "openstack"
+virsh vol-delete --pool HP-Disk openstack
+
+IFS=
+kickstart_file=centos-8-kickstart-openstack.cfg
+####initial certs###############
+letsEncryptAndCockpitCerts ${kickstart_file}
+###############################
+
+########### add passwords in
+sed -i 's/{GITHUB_TOKEN}/'$GITHUB_TOKEN'/g' ${kickstart_file}
+sed -i 's/{CENTOS_ROOT_PWD}/'$CENTOS_ROOT_PWD'/g' ${kickstart_file}
+###########################
+
+############### Secrets file ################
+echo 'cat > /tmp/openstack-env.sh <<EOF' >> ${kickstart_file}
+cat ./openstack-env.sh >> ${kickstart_file}
+echo 'EOF' >> ${kickstart_file}
+###############################
+
+closeOutAndBuildKickstartAndISO "${kickstart_file}" "openstack"
+
+echo "virt-install --virt-type kvm --name openstack --memory 160000 --vcpus 2 --disk pool=HP-Disk,size=400,bus=scsi --cdrom /var/tmp/openstack-iso.iso --network bridge=os-int-static,model=rtl8139 --network bridge=os-loc-static,model=rtl8139 --os-variant centos8 --graphics vnc"
+
+eval "virt-install --virt-type kvm --name openstack --memory 160000 --vcpus 2 --disk pool=HP-Disk,size=400,bus=scsi --cdrom /var/tmp/openstack-iso.iso --network bridge=os-int-static,model=rtl8139 --network bridge=os-loc-static,model=rtl8139 --os-variant centos8 --graphics vnc" &
+
---
+++ toolbox-kvm-openstack.sh
@@ -0,0 +1,14 @@
+#can ONLY be run as root!  sudo to root
+source ./openstack-setup/openstack-env.sh
+
+rm -rf /tmp/openstack-setup;
+rm -rf /tmp/openstack-scripts;
+
+git clone https://mephmanx:$GITHUB_TOKEN@github.com/mephmanx/openstack-setup.git /tmp/openstack-setup;
+git clone https://mephmanx:$GITHUB_TOKEN@github.com/mephmanx/openstack-scripts.git /tmp/openstack-scripts;
+
+cp /tmp/openstack-scripts/*.sh /tmp/openstack-setup;
+cp /tmp/openstack-scripts/*.cfg /tmp/openstack-setup;
+
+cd /tmp/openstack-setup
+/tmp/openstack-setup/create-openstack-centos8-kvm.sh
