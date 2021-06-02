9ff1ef22bed1dd351db819b17273da03832df7cb
Chris Lyons <chris@lyonsgroup.family>
2021-06-02 18:22:36 -0400

    adding openstack server

--- vmware-centos8-openstack.sh
+++ vmware-centos8-openstack.sh
@@ -26,4 +26,49 @@ chmod +x /etc/rc.d/rc.local
 
 ########################
 
+
+################# Bond all NIC's together
+#export IP=`hostname -I | awk '{print $1}'`
+#export IP+="/24"
+nmcli connection add type bond con-name os-int-static ifname os-int-static mode 802.3ad
+nmcli con mod id os-int-static bond.options mode=802.3ad,miimon=100,lacp_rate=fast,xmit_hash_policy=layer2+3
+
+nmcli con mod os-int-static ipv4.method auto
+nmcli con mod os-int-static ipv6.method dhcp
+nmcli con mod os-int-static ipv4.never-default no
+nmcli con mod os-int-static ipv4.dhcp-fqdn `hostname`.$DOMAIN_NAME
+nmcli con mod os-int-static connection.autoconnect yes
+
+ct=0
+for DEVICE in `nmcli device | awk '$1 != "DEVICE" && $3 == "connected" && $2 == "ethernet" { print $1 }'`; do
+    echo "$DEVICE"
+    nmcli connection delete $DEVICE
+    nmcli con add type bond-slave con-name os-int-static-slave$ct ifname $DEVICE master os-int-static
+    ((ct++))
+done
+
+nmcli connection down os-int-static && nmcli connection up os-int-static
+##########################################
+
+################# Add bridge
+#ip link add os-int-static type bridge
+#ip address add dev os-int-static $IP
+#ip link set bond0 master os-int-static
+#
+cat > /etc/sysctl.d/99-netfilter-bridge.conf <<EOF
+net.bridge.bridge-nf-call-ip6tables = 0
+net.bridge.bridge-nf-call-iptables = 0
+net.bridge.bridge-nf-call-arptables = 0
+EOF
+
+modprobe br_netfilter
+
+cat > /etc/modules-load.d/br_netfilter.conf <<EOF
+br_netfilter
+EOF
+
+sysctl -p /etc/sysctl.d/99-netfilter-bridge.conf
+###########################
+
+
 reboot
