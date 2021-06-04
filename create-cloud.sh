95c51e8fbab2eea13be875e388895a69cef52d43
Chris Lyons <chris@lyonsgroup.family>
2021-06-04 09:01:07 -0400

    adding openstack server

--- create-cloud.sh
+++ create-cloud.sh
@@ -131,13 +131,13 @@ virsh undefine "kolla"
 
 ########### Delete volumes in storage pools
 virsh vol-list HP-Disk | awk 'NR > 2 && !/^+--/ { print $1 }' | while read line; do
-  if [[ -z "$line" ]]; then
+  if [[ ! -z $line ]]; then
     virsh vol-delete --pool HP-Disk $line
   fi
 done
 
 virsh vol-list HP-SSD | awk 'NR > 2 && !/^+--/ { print $1 }' | while read line; do
-  if [[ -z "$line" ]]; then
+  if [[ ! -z $line ]]; then
     virsh vol-delete --pool HP-SSD $line
   fi
 done
