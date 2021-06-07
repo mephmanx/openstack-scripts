8cec342b595bf0172eb8e4730819a3f5f66afd72
Chris Lyons <chris@lyonsgroup.family>
2021-06-06 20:05:28 -0400

    adding openstack server

--- openstack.sh
+++ openstack.sh
@@ -20,27 +20,6 @@ systemctl stop firewalld
 systemctl disable firewalld
 systemctl mask firewalld
 
-################# Bond all NIC's together
-#export IP=`hostname -I | awk '{print $1}'`
-#export IP+="/24"
-nmcli connection add type bond con-name os-int-static ifname os-int-static mode 802.3ad
-nmcli con mod id os-int-static bond.options mode=802.3ad,miimon=100,lacp_rate=fast,xmit_hash_policy=layer2+3
-
-nmcli con mod os-int-static ipv4.method auto
-nmcli con mod os-int-static ipv6.method auto
-nmcli con mod os-int-static connection.autoconnect yes
-
-ct=0
-for DEVICE in `nmcli device | awk '$1 != "DEVICE" && $3 == "connected" && $2 == "ethernet" { print $1 }'`; do
-    echo "$DEVICE"
-    nmcli connection delete $DEVICE
-    nmcli con add type bond-slave con-name os-int-static-slave$ct ifname $DEVICE master os-int-static
-    ((ct++))
-done
-
-nmcli connection down os-int-static && nmcli connection up os-int-static
-##########################################
-
 ################# setup KVM and kick off openstack cloud create
 dnf module install -y virt
 dnf install -y cockpit-machines virt-install virt-viewer
