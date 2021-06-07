94cc0248f332888d70a85b79f517b5aa5e487a11
Chris Lyons <chris@lyonsgroup.family>
2021-06-07 19:13:04 -0400

    adding openstack server

--- vm-configurations.sh
+++ vm-configurations.sh
@@ -134,7 +134,11 @@ function create_vm_kvm {
   IFS=',' read -r -a net_array <<< "$network_string"
   for net_element in "${net_array[@]}"
     do
-      virt_network_list+=("--network type=direct,source=$net_element,model=virtio ")
+      if [[ "${net_element}" =~ .*"loc".* ]]; then
+        virt_network_list+=("--network type=network,source=$net_element,model=virtio ")
+      else
+        virt_network_list+=("--network type=direct,source=$net_element,model=virtio ")
+      fi
   done
   #########################
 
