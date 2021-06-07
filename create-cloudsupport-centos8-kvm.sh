0640348b09944133d6e6ac030190a35037076a2a
Chris Lyons <chris@lyonsgroup.family>
2021-06-07 19:59:07 -0400

    adding openstack server

--- create-cloudsupport-centos8-kvm.sh
+++ create-cloudsupport-centos8-kvm.sh
@@ -11,7 +11,7 @@ cp /tmp/openstack-scripts/*.cfg /tmp/openstack-setup;
 
 virsh destroy "cloudsupport"
 virsh undefine "cloudsupport"
-virsh vol-delete --pool HP-Disk cloudsupport
+virsh vol-delete --pool HP-EXT cloudsupport
 
 IFS=
 kickstart_file=centos-8-kickstart-cloudsupport.cfg
