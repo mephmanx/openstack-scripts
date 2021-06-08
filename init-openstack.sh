ebeada8e32c0c5bfd11348ba5ea68affcb91a4b4
Chris Lyons <chris@lyonsgroup.family>
2021-06-08 16:44:01 -0400

    adding openstack server

--- init-openstack.sh
+++ init-openstack.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# chkconfig: 2345 20 80
+# description: Description comes here....
+# ALWAYS have content in functions otherwise you get syntax errors!
 
 # Source function library.
+. /etc/init.d/functions
 . /tmp/vm_functions.sh
 
+start() {
 
 exec 1>/tmp/start-install.log 2>&1 # send stdout and stderr from rc.local to a log file
 set -x                             # tell sh to display commands before execution
@@ -12,16 +17,6 @@ sleep 30
 chmod 777 /tmp/openstack-env.sh
 source ./tmp/openstack-env.sh
 ############################
-
-systemctl start cockpit.socket
-systemctl enable --now cockpit.socket
-
-############## Prep OpenStack install
-rm -rf /etc/rc.d/rc.local
-curl -o /etc/rc.d/rc.local https://mephmanx:$GITHUB_TOKEN@raw.githubusercontent.com/mephmanx/openstack-scripts/master/openstack.sh
-chmod +x /etc/rc.d/rc.local
-
-########################
 
 ################# Bond all NIC's together
 nmcli connection add type bond con-name int-static ifname int-static mode 802.3ad
@@ -42,4 +37,38 @@ done
 nmcli connection down int-static && nmcli connection up int-static
 ##########################################
 
+# set up net script to be called after reboot
+prep_next_script "openstack"
+
 reboot
+
+}
+
+stop() {
+    # code to stop app comes here
+    # example: killproc program_name
+    /bin/true
+}
+
+case "$1" in
+    start)
+       start
+       ;;
+    stop)
+        /bin/true
+       stop
+       ;;
+    restart)
+       stop
+       start
+       ;;
+    status)
+        /bin/true
+       # code to check status of app comes here
+       # example: status program_name
+       ;;
+    *)
+       echo "Usage: $0 {start|stop|status|restart}"
+esac
+
+exit 0
