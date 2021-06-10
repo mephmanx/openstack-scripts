4a6852a12eaece94e99352659664b9386ae7cf41
Chris Lyons <chris@lyonsgroup.family>
2021-06-10 19:15:19 -0400

    adding openstack server

--- vm-configurations.sh
+++ vm-configurations.sh
@@ -125,7 +125,7 @@ function create_vm_kvm {
   for element in "${disk_array[@]}"
     do
       IFS=':' read -ra drive_info <<< "$element"
-      virt_disk_list+=("--disk pool=${drive_info[0]},size=${drive_info[1]},bus=scsi,sparse=no,cache=writeback ")
+      virt_disk_list+=("--disk pool=${drive_info[0]},size=${drive_info[1]},bus=scsi,sparse=no ")
   done
   #####################
 
