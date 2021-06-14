0e90f440de95b6c845d8221cb914a9a9cc1ee4ec
Chris Lyons <chris@lyonsgroup.family>
2021-06-13 23:25:02 -0400

    adding openstack server

--- kolla.sh
+++ kolla.sh
@@ -10,6 +10,73 @@ cd /tmp
 common_second_boot_setup
 #################
 
+########### set up registry connection to docker hub
+export etext=`echo -n "$SUPPORT_USERNAME:$SUPPORT_PASSWORD" | base64`
+curl -k --location --request POST "https://10.0.20.200/api/v2.0/registries" \
+  --header "authorization: Basic $etext" \
+  --header 'content-type: application/json' \
+  --header 'host: cloudsupport.lyonsgroup.family' \
+  -d @- <<'EOF'
+{
+    "status": "healthy",
+    "credential": {
+        "access_key": "$DOCKER_HUB_PWD",
+        "access_secret": "$DOCKER_HUB_USER",
+        "type": "basic"
+    },
+    "update_time": "2021-06-14T01:52:35.36006Z",
+    "name": "docker-hub",
+    "url": "https://hub.docker.com",
+    "insecure": false,
+    "creation_time": "2021-06-14T01:52:35.360055Z",
+    "type": "docker-hub",
+    "id": 0,
+    "description": "docker hub"
+}
+EOF
+###########################
+
+###########  remove default "library" project and create new proxy-cache library project
+curl -k --location --request DELETE "https://10.0.20.200/api/v2.0/projects/1" \
+  --header "authorization: Basic $etext"
+
+curl -k --location --request POST "https://10.0.20.200/api/v2.0/projects" \
+  --header "authorization: Basic $etext" \
+  --header 'content-type: application/json' \
+  --header 'host: cloudsupport.lyonsgroup.family' \
+  -d @- <<'EOF'
+{
+  "project_name": "library",
+  "cve_allowlist": {
+      "items": [
+          {
+              "cve_id": "string"
+          }
+      ],
+      "project_id": 0,
+      "id": 0,
+      "expires_at": 0,
+      "update_time": "2021-06-12T15:44:26.510Z",
+      "creation_time": "2021-06-12T15:44:26.510Z"
+  },
+  "count_limit": 0,
+  "registry_id": 1,
+  "storage_limit": 0,
+  "metadata": {
+      "enable_content_trust": "string",
+      "auto_scan": "string",
+      "severity": "string",
+      "public": "string",
+      "reuse_sys_cve_allowlist": "string",
+      "prevent_vul": "string",
+      "retention_id": "string"
+  },
+  "public": true,
+  "proxy_cache": true
+}
+EOF
+
+
 unset HOME
 
 mkdir /home/stack
