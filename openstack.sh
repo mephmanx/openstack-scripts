fb24739889e3a8e82f7f3105b05599388f5a0423
Chris Lyons <chris@lyonsgroup.family>
2021-06-15 10:07:01 -0400

    adding openstack server

--- openstack.sh
+++ openstack.sh
@@ -28,7 +28,13 @@ systemctl restart libvirtd
 
 ### enable nested virtualization
 sed -i "s/#options kvm_intel nested=1/options kvm_intel nested=1/g" /etc/modprobe.d/kvm.conf
+echo "options kvm-intel enable_shadow_vmcs=1" >> /etc/modprobe.d/kvm.conf
+echo "options kvm-intel enable_apicv=1" >> /etc/modprobe.d/kvm.conf
+echo "options kvm-intel ept=1" >> /etc/modprobe.d/kvm.conf
 modprobe kvm_intel nested=1
+modprobe kvm_intel enable_shadow_vmcs=1
+modprobe kvm_intel enable_apicv=1
+modprobe kvm_intel ept=1
 ##############
 
 ################# Add bridge
