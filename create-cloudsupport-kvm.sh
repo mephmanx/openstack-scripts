9a3143bef17ac204803695e8b217c728d8061320
Chris Lyons <chris@lyonsgroup.family>
2021-06-16 21:57:33 -0400

    adding openstack server

--- create-cloudsupport-kvm.sh
+++ create-cloudsupport-kvm.sh
@@ -22,7 +22,7 @@ echo 'EOF' >> ${kickstart_file}
 
 closeOutAndBuildKickstartAndISO "${kickstart_file}" "cloudsupport"
 
-echo "virt-install --virt-type kvm --name cloudsupport --memory 16000 --hvm --cpu host-passthrough,cache.mode=passthrough --vcpus 2 --disk pool=HP-EXT,size=400,bus=scsi,sparse=no --cdrom /var/tmp/cloudsupport-iso.iso --network type=direct,source=int-static,model=virtio  --network type=network,source=loc-static,model=virtio --os-variant centos8 --graphics vnc --autostart"
+echo "virt-install --virt-type kvm --name cloudsupport --memory 16000 --hvm --cpu host-passthrough,cache.mode=passthrough --vcpus 2,maxvcpus=8,sockets=2,cores=1,threads=4 --disk pool=HP-EXT,size=400,bus=scsi,sparse=no --cdrom /var/tmp/cloudsupport-iso.iso --network type=direct,source=int-static,model=virtio  --network type=network,source=loc-static,model=virtio --os-variant centos8 --graphics vnc --autostart"
 
-eval "virt-install --virt-type kvm --name cloudsupport --memory 16000 --hvm --cpu host-passthrough,cache.mode=passthrough --vcpus 2 --disk pool=HP-EXT,size=400,bus=scsi,sparse=no --cdrom /var/tmp/cloudsupport-iso.iso --network type=direct,source=int-static,model=virtio  --network type=network,source=loc-static,model=virtio --os-variant centos8 --graphics vnc --autostart" &
+eval "virt-install --virt-type kvm --name cloudsupport --memory 16000 --hvm --cpu host-passthrough,cache.mode=passthrough --vcpus 2,maxvcpus=8,sockets=2,cores=1,threads=4 --disk pool=HP-EXT,size=400,bus=scsi,sparse=no --cdrom /var/tmp/cloudsupport-iso.iso --network type=direct,source=int-static,model=virtio  --network type=network,source=loc-static,model=virtio --os-variant centos8 --graphics vnc --autostart" &
 
