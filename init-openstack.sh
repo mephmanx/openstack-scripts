20ea093f93cf216a5014f2f034e3d80ca06828c1
Chris Lyons <chris@lyonsgroup.family>
2021-06-21 14:04:23 -0400

    adding openstack server

--- init-openstack.sh
+++ init-openstack.sh
@@ -22,6 +22,7 @@ load_secrets
 chmod 777 /tmp/global_addresses.sh
 
 #####  get HPE driver/software repo to pull latest drivers
+####  Only if running on HP hardware!
 cat > /etc/yum.repos.d/hp.repo <<EOF
 [HP-spp]
 name=HP Service Pack for ProLiant
@@ -36,10 +37,18 @@ baseurl=http://downloads.linux.hpe.com/SDR/repo/mcp/centos/8.1/x86_64/current/
 enabled=1
 gpgcheck=0
 gpgkey=file:///etc/pki/rpm-gpg/GPG-KEY-mcp
+
+[HP-sum]
+name=HP Smart Update Manager
+baseurl=http://downloads.linux.hpe.com/repo/hpsum/rhel/8/x86_64/current/
+enabled=1
+gpgcheck=0
+gpgkey=file:///etc/pki/rpm-gpg/GPG-KEY-hpsum
 EOF
 
 wget http://downloads.linux.hpe.com/SDR/repo/spp/GPG-KEY-spp -O /etc/pki/rpm-gpg/GPG-KEY-spp
 wget http://downloads.linux.hpe.com/SDR/repo/mcp/GPG-KEY-mcp -O /etc/pki/rpm-gpg/GPG-KEY-mcp
+wget http://downloads.linux.hpe.com/SDR/repo/hpsum/GPG-KEY-mcp -O /etc/pki/rpm-gpg/GPG-KEY-hpsum
 
 dnf --enablerepo=powertools install libidn-devel
 dnf install libnsl
