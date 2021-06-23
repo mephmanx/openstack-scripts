808bba16d305160d77343cd5923702cdd3e26c7c
Chris Lyons <chris@lyonsgroup.family>
2021-06-23 12:20:14 -0400

    adding openstack server

--- control.sh
+++ control.sh
@@ -14,16 +14,7 @@ start() {
 common_second_boot_setup
 
 ######## Put type specific code
-#runuser -l root -c  '/sbin/ip link set eth2 promisc on'
-#runuser -l root -c  '/sbin/ip link set eth0 promisc on'
 
-#sed '/^IPADDR/d' -i /tmp/eth2
-#sed '/^GATEWAY/d' -i /tmp/eth2
-#sed '/^DNS1/d' -i /tmp/eth2
-#sed '/^NETMASK/d' -i /tmp/eth2
-#
-#runuser -l root -c  "rm -rf /etc/sysconfig/network-scripts/ifcfg-eth2"
-#runuser -l root -c  "cat /tmp/eth2 > /etc/sysconfig/network-scripts/ifcfg-eth2"
 ############################
 
 #remove so as to not run again
