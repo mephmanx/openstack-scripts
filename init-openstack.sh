6498a4e5896d41ba742fe3c4a6545edb2820bfde
Chris Lyons <chris@lyonsgroup.family>
2021-06-26 19:38:12 -0400

    adding openstack server

--- init-openstack.sh
+++ init-openstack.sh
@@ -63,12 +63,9 @@ nmcli connection down int-static && nmcli connection up int-static
 nmcli connection add type bond con-name loc-static ifname loc-static mode balance-rr
 nmcli con mod id loc-static bond.options mode=balance-rr,miimon=100
 
-nmcli con mod loc-static ipv4.addresses 10.0.20.2/24
-nmcli con mod loc-static ipv4.method manual
-nmcli con mod loc-static ipv4.gateway 10.0.20.1
-nmcli con mod loc-static ipv4.dns 10.0.20.1
-nmcli con mod loc-static ipv6.method auto
-nmcli con mod loc-static connection.autoconnect yes
+nmcli con mod int-static ipv4.method auto
+nmcli con mod int-static ipv6.method auto
+nmcli con mod int-static connection.autoconnect yes
 
 ct=0
 for DEVICE in `nmcli device | awk '$1 != "DEVICE" && $3 == "connected" && $2 == "ethernet" { print $1 }'`; do
