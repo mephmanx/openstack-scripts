101dc7a8953854a41b2ea36785410d160a95ad14
Chris Lyons <chris@lyonsgroup.family>
2021-07-04 17:38:07 -0400

    adding openstack server

--- openstack.sh
+++ openstack.sh
@@ -157,7 +157,6 @@ cd ../swtpm \
 
 runuser -l root -c  'cd /usr/share/swtpm; ./swtpm-create-user-config-files --overwrite --root;'
 runuser -l root -c  'chown tss:tss /root/.config/*'
-#runuser -l root -c  'chown tss:tss /var/lib/swtpm-localca/*'
 #####################
 
 ############ Create and init storage pools
