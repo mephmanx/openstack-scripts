8840837882926560e0cbff4e751d91454f4bf8a3
Chris Lyons <chris@lyonsgroup.family>
2021-07-08 14:24:50 -0400

    Revert "adding openstack server"
    This reverts commit 2c968333

--- compute.sh
+++ compute.sh
@@ -23,6 +23,40 @@ sed -i '/^NETMASK/d' /etc/sysconfig/network-scripts/ifcfg-eth1
 sed -i '/^GATEWAY/d' /etc/sysconfig/network-scripts/ifcfg-eth1
 ############################
 
+###### vTPM setup #####
+cd /root
+dnf -y update \
+ && dnf -y install diffutils make file automake autoconf libtool gcc gcc-c++ openssl-devel gawk git \
+ && git clone https://github.com/stefanberger/libtpms.git \
+ && dnf -y install which python3 python3-cryptography python3-pip python3-setuptools expect libtasn1-devel \
+    socat trousers tpm-tools gnutls-devel gnutls-utils net-tools libseccomp-devel json-glib-devel \
+ && pip3 install twisted \
+ && git clone https://github.com/stefanberger/swtpm.git
+
+LIBTPMS_BRANCH=master
+
+cd libtpms \
+ && runuser -l root -c  'echo ${date} > /.date' \
+ && git pull \
+ && git checkout ${LIBTPMS_BRANCH} \
+ && runuser -l root -c  'cd libtpms; ./autogen.sh --prefix=/usr --libdir=/usr/lib64 --with-openssl --with-tpm2;' \
+ && make -j$(nproc) V=1 \
+ && make -j$(nproc) V=1 check \
+ && make install
+
+SWTPM_BRANCH=master
+cd ../swtpm \
+ && git pull \
+ && git checkout ${SWTPM_BRANCH} \
+ && runuser -l root -c  'cd /root/swtpm; ./autogen.sh --prefix=/usr --libdir=/usr/lib/x86_64-linux-gnu --with-openssl;' \
+ && make -j$(nproc) V=1 \
+ && make -j$(nproc) V=1 VERBOSE=1 check \
+ && make -j$(nproc) install
+
+runuser -l root -c  'cd /usr/share/swtpm; ./swtpm-create-user-config-files --overwrite --root;'
+runuser -l root -c  'chown tss:tss /root/.config/*'
+#####################
+
 #remove so as to not run again
 rm -rf /etc/rc.d/rc.local
 
