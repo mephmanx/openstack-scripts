3e5ce4aa9f9f8e7c1d9bda33adfde217ae53b6bb
Chris Lyons <chris@lyonsgroup.family>
2021-07-11 17:40:32 -0400

    Revert "adding openstack server"
    This reverts commit 2c968333

--- vm-configurations.sh
+++ vm-configurations.sh
@@ -36,7 +36,7 @@ function vm_definitions {
             "cpu":"2",
             "memory":"34",
             "drive_string":"HP-Disk:100",
-            "network_string":"loc-static,int-static"
+            "network_string":"loc1-static,loc-static2"
           }'
     ;;
     "network")
@@ -45,7 +45,7 @@ function vm_definitions {
             "cpu":"2",
             "memory":"12",
             "drive_string":"HP-Disk:100",
-            "network_string":"loc-static,loc-static,int-static"
+            "network_string":"loc-static1,loc-static2"
           }'
     ;;
     "compute")
@@ -54,7 +54,7 @@ function vm_definitions {
             "cpu":"24",
             "memory":180",
             "drive_string":"HP-SSD:700",
-            "network_string":"loc-static,int-static,int-static"
+            "network_string":"loc-static1,loc-static2"
           }'
     ;;
     "monitoring")
@@ -63,7 +63,7 @@ function vm_definitions {
             "cpu":"2",
             "memory":"20",
             "drive_string":"HP-Disk:300",
-            "network_string":"loc-static,int-static"
+            "network_string":"loc-static1,loc-static2"
           }'
     ;;
     "storage")
@@ -72,7 +72,7 @@ function vm_definitions {
             "cpu":"2",
             "memory":"22",
             "drive_string":"HP-Disk:300,HP-Disk:300,HP-SSD:175,HP-SSD:175,HP-SSD:175",
-            "network_string":"loc-static,int-static"
+            "network_string":"loc-static1,loc-static2"
           }'
     ;;
     "kolla")
@@ -81,7 +81,7 @@ function vm_definitions {
             "cpu":"4",
             "memory":"8",
             "drive_string":"HP-Disk:100",
-            "network_string":"loc-static,int-static"
+            "network_string":"loc-static1,loc-static2"
           }'
     ;;
   esac
