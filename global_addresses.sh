c0250dace3aac0ea0b4c1fc27f413c75a70d9324
Chris Lyons <chris@lyonsgroup.family>
2021-07-12 22:36:21 -0400

    Revert "adding openstack server"
    This reverts commit 2c968333

--- global_addresses.sh
+++ global_addresses.sh
@@ -2,7 +2,7 @@
 
 export DOMAIN_NAME=lyonsgroup.family;
 export APP_INTERNAL_HOSTNAME=openstack-local;
-export APP_EXTERNAL_HOSTNAME=openstack;
+export APP_EXTERNAL_HOSTNAME=openstack-external;
 export SUPPORT_HOST=cloudsupport;
 export INTERNAL_VIP="10.0.20.254";
 export EXTERNAL_VIP="10.0.21.254";
