4189639eb1455d4c5a05518328e04540c7cd7b36
Chris Lyons <chris@lyonsgroup.family>
2021-07-16 07:56:43 -0400

    Revert "adding openstack server"
    This reverts commit 2c968333

--- create-openstack-kvm.sh
+++ create-openstack-kvm.sh
@@ -31,20 +31,3 @@ echo 'EOF' >> ${kickstart_file}
 
 closeOutAndBuildKickstartAndISO "${kickstart_file}" "openstack"
 
-create_line="virt-install "
-create_line+="--hvm "
-create_line+="--virt-type=kvm "
-create_line+="--name=openstack "
-create_line+="--memory=16000 "
-create_line+="--cpu=host-passthrough,cache.mode=passthrough "
-create_line+="--cpuset=auto "
-create_line+="--vcpus=4,maxvcpus=4,sockets=2,cores=1,threads=2 "
-create_line+="--disk=pool=HP-Disk,size=400,bus=scsi,sparse=no "
-create_line+="--cdrom=/var/tmp/openstack-iso.iso "
-create_line+="--network type=network,source=loc-static,model=virtio "
-create_line+="--os-variant=centos8 "
-create_line+="--graphics=vnc "
-create_line+="--autostart"
-
-echo $create_line
-eval $create_line &
