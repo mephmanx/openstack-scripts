3327d1c04fcf7e534bf21bc343e5eb29b3cd7e4b
Chris Lyons <chris@lyonsgroup.family>
2021-07-16 12:54:04 -0400

    Revert "adding openstack server"
    This reverts commit 2c968333

--- toolbox-kvm-openstack.sh
+++ toolbox-kvm-openstack.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #can ONLY be run as root!  sudo to root
-source ./openstack-setup/openstack-env.sh
+
+source ./iso-functions.sh
+source ./openstack-env.sh
 
 rm -rf /tmp/openstack-setup;
 rm -rf /tmp/openstack-scripts;
@@ -13,4 +15,30 @@ cp /tmp/openstack-scripts/*.cfg /tmp/openstack-setup;
 
 rm -rf /var/tmp/openstack-iso.iso
 cd /tmp/openstack-setup
-/tmp/openstack-setup/create-openstack-kvm.sh &
+
+IFS=
+kickstart_file=centos-8-kickstart-openstack.cfg
+####initial certs###############
+letsEncryptAndCockpitCerts ${kickstart_file}
+###############################
+
+########### add passwords in
+sed -i 's/{GITHUB_TOKEN}/'$GITHUB_TOKEN'/g' ${kickstart_file}
+sed -i 's/{CENTOS_ROOT_PWD}/'$CENTOS_ROOT_PWD'/g' ${kickstart_file}
+###########################
+
+############### Secrets file ################
+echo 'cat > /tmp/openstack-env.sh <<EOF' >> ${kickstart_file}
+cat ./openstack-env.sh >> ${kickstart_file}
+echo 'EOF' >> ${kickstart_file}
+###############################
+
+############### Global Addresses ################
+echo 'cat > /tmp/global_addresses.sh <<EOF' >> ${kickstart_file}
+cat ./global_addresses.sh >> ${kickstart_file}
+echo 'EOF' >> ${kickstart_file}
+###############################
+
+closeOutAndBuildKickstartAndISO "${kickstart_file}" "openstack"
+
+
