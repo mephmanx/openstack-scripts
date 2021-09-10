dfd0b99e464fde6f22ab7d35c26b30104b7f2349
Chris Lyons <chris@lyonsgroup.family>
2021-09-10 16:21:55 -0400

    removing openstack-setup repo

--- toolbox-kvm-cloud.sh
+++ toolbox-kvm-cloud.sh
@@ -5,21 +5,15 @@ exec 1>/root/cloud-install.log 2>&1 # send stdout and stderr from rc.local to a 
 set -x
 
 source /tmp/openstack-env.sh
-
-rm -rf /tmp/openstack-setup;
 rm -rf /tmp/openstack-scripts;
 
-git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_USER/openstack-setup.git /tmp/openstack-setup;
 git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_USER/openstack-scripts.git /tmp/openstack-scripts;
 
-cp /tmp/openstack-scripts/*.sh /tmp/openstack-setup;
-cp /tmp/openstack-scripts/*.cfg /tmp/openstack-setup;
-
 ## prep project config by replacing nested vars
-cp /tmp/openstack-setup/vm_functions.sh /tmp/vm_functions.sh
-source /tmp/openstack-setup/vm_functions.sh
+cp /tmp/openstack-scripts/vm_functions.sh /tmp/vm_functions.sh
+source /tmp/openstack-scripts/vm_functions.sh
 prep_project_config
 #########
 
-cd /tmp/openstack-setup
-/tmp/openstack-setup/create-cloud-kvm.sh &
+cd /tmp/openstack-scripts
+/tmp/openstack-scripts/create-cloud-kvm.sh &
--- toolbox-kvm-openstack.sh
+++ toolbox-kvm-openstack.sh
@@ -1,31 +1,23 @@
 #!/bin/bash
 #can ONLY be run as root!  sudo to root
 
-source /tmp/openstack-setup/openstack-env.sh
+### openstack-env needs to be in same directory as this script
+source ./openstack-env.sh
 
-rm -rf /tmp/openstack-setup;
 rm -rf /tmp/openstack-scripts;
-
-git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_USER/openstack-setup.git /tmp/openstack-setup;
 git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_USER/openstack-scripts.git /tmp/openstack-scripts;
 
-cp /tmp/openstack-scripts/*.sh /tmp/openstack-setup;
-cp /tmp/openstack-scripts/*.cfg /tmp/openstack-setup;
-
 rm -rf /tmp/vm_functions.sh
-rm -rf /tmp/openstack-env.sh
 cp /tmp/openstack-scripts/vm_functions.sh /tmp
-cp /tmp/openstack-setup/openstack-env.sh /tmp
 
 rm -rf /var/tmp/openstack-iso.iso
-cd /tmp/openstack-setup
 
 ## prep project config by replacing nested vars
 source /tmp/vm_functions.sh
 prep_project_config
 #########
 
-source /tmp/openstack-setup/iso-functions.sh
+source ./iso-functions.sh
 source /tmp/project_config.sh
 
 IFS=
@@ -33,8 +25,8 @@ kickstart_file=centos-8-kickstart-openstack.cfg
 
 #### generate random password and reload env
 export RANDOM_PWD=`date +%s | sha256sum | base64 | head -c 32 ; echo`
-sed -i 's/{RANDOM_PWD}/'$RANDOM_PWD'/g' /tmp/openstack-setup/openstack-env.sh
-source /tmp/openstack-setup/openstack-env.sh
+sed -i 's/{RANDOM_PWD}/'$RANDOM_PWD'/g' ./openstack-env.sh
+source ./openstack-env.sh
 ######
 
 ########### add passwords in
@@ -46,7 +38,7 @@ sed -i 's/{TIMEZONE}/'$TIMEZONE'/g' ${kickstart_file}
 
 ############### Secrets file ################
 echo 'cat > /tmp/openstack-env.sh <<EOF' >> ${kickstart_file}
-cat /tmp/openstack-setup/openstack-env.sh >> ${kickstart_file}
+cat ./openstack-env.sh >> ${kickstart_file}
 echo 'EOF' >> ${kickstart_file}
 ###############################
 
