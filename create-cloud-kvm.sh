6758e03b116c0b61ff4a130160dc8a710adbcaa0
Chris Lyons <chris@lyonsgroup.family>
2021-09-13 10:49:23 -0400

    moving generated passwords to internal operation

--- create-cloud-kvm.sh
+++ create-cloud-kvm.sh
@@ -41,7 +41,7 @@ SUPPORT_VIP_DNS="$SUPPORT_HOST.$DOMAIN_NAME"
 ##############################################
 
 #### setup static network local DNS entries
-echo "runuser -l root -c  'echo "$EXTERNAL_VIP $EXTERNAL_VIP_DNS" >> /etc/hosts;'" >> /tmp/dns_hosts
+#echo "runuser -l root -c  'echo "$EXTERNAL_VIP $EXTERNAL_VIP_DNS" >> /etc/hosts;'" >> /tmp/dns_hosts
 echo "runuser -l root -c  'echo "$INTERNAL_VIP $INTERNAL_VIP_DNS" >> /etc/hosts;'" >> /tmp/dns_hosts
 ####  make sure to use an in-memory network for docker pull through cache otherwise 500's occur
 echo "runuser -l root -c  'echo "$SUPPORT_VIP $SUPPORT_VIP_DNS" >> /etc/hosts;'" >> /tmp/dns_hosts
