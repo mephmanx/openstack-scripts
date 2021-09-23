7980939ce0ede62aab6cc01de3d6d468e8aae952
Chris Lyons <chris@lyonsgroup.family>
2021-09-23 16:39:34 -0400

    updating endpoints to be reflective of systems

--- vm-configurations.sh
+++ vm-configurations.sh
@@ -50,7 +50,7 @@ function getDriveRatings() {
 
 function getFastestDrive() {
   drive_speed_string=$1
-  IFS=',' read -r -a drive_ratings <<< "$drive_speed_string"
+  IFS=' ' read -r -a drive_ratings <<< "$drive_speed_string"
   fastest_drive_speed=0
   for entry in "${drive_ratings[@]}"; do
     if [[ $(round $(cut -d':' -f2 <<<$entry) 0) -gt $fastest_drive_speed ]]; then
@@ -65,7 +65,7 @@ function getSecondFastestDrive() {
   drive_speed_string=$1
   ## remove fastest drive info
   fDr=$(getFastestDrive $drive_speed_string)
-  IFS=',' read -r -a drive_ratings <<< "$drive_speed_string"
+  IFS=' ' read -r -a drive_ratings <<< "$drive_speed_string"
   new_arr=()
   for ele in "${drive_ratings[@]}"; do
     if [[ "$ele" != "$fDr"* ]]; then
