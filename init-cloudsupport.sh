2006e4b6111bcd63159ab9982e5993072d7539ca
Chris Lyons <chris@lyonsgroup.family>
2021-10-04 19:05:25 -0400

    opening ssh to kolla

--- init-cloudsupport.sh
+++ init-cloudsupport.sh
@@ -23,11 +23,6 @@ sleep 30
 rm -rf /tmp/eth*
 ########
 
-## trust generated ca
-cp /etc/cockpit/ws-certs.d/certificate.cert /etc/pki/ca-trust/source/anchors
-runuser -l root -c  'update-ca-trust extract'
-#########
-
 ## enable auto updates if selected
 if [[ $LINUX_AUTOUPDATE == 1 ]]; then
   systemctl enable --now dnf-automatic.timer
