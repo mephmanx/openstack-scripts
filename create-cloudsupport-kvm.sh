b863946048106239352a74a68a86ca475c5c9a80
Chris Lyons <chris@lyonsgroup.family>
2021-10-04 16:28:08 -0400

    opening ssh to kolla

--- create-cloudsupport-kvm.sh
+++ create-cloudsupport-kvm.sh
@@ -13,7 +13,7 @@ removeVM_kvm "cloudsupport"
 
 IFS=
 rm -rf ${KICKSTART_DIR}/centos-8-kickstart-cs.cfg
-cp ${KICKSTART_DIR}/centos-8-kickstart-cloud_common.cfg ${KICKSTART_DIR}/centos-8-kickstart-cs.cfg
+cp ${KICKSTART_DIR}/centos-8-kickstart-cloudsupport.cfg ${KICKSTART_DIR}/centos-8-kickstart-cs.cfg
 echo "copied kickstart -> ${KICKSTART_DIR}/centos-8-kickstart-cloud_common.cfg to -> ${KICKSTART_DIR}/centos-8-kickstart-cs.cfg"
 kickstart_file=${KICKSTART_DIR}/centos-8-kickstart-cs.cfg
 echo "kickstart file -> ${kickstart_file}"
