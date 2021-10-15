0066428f51b5525828451b317dd601b47eaeea98
Chris Lyons <chris@lyonsgroup.family>
2021-10-15 15:50:28 -0400

    trove setup

--- kolla.sh
+++ kolla.sh
@@ -571,7 +571,7 @@ openstack image create amphora-x64-haproxy \
   --disk-format qcow2 \
   --tag amphora \
   --private \
-  --file /root/amphora-x64-haproxy.qcow2 \
+  --file /tmp/octavia/diskimage-create/amphora-x64-haproxy.qcow2 \
   --property hw_architecture='x86_64' \
   --property hw_rng_model=virtio
 
