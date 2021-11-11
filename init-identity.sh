587c73c1d95c1d152c8a4c45500b0074f7dee410
Chris Lyons <chris@lyonsgroup.family>
2021-11-10 21:27:09 -0500

    changing DNS from pfsense to identity

--- init-identity.sh
+++ init-identity.sh
@@ -92,6 +92,7 @@ runuser -l root -c "ipa-dns-install --auto-forwarders --no-reverse --no-dnssec-v
 echo $ADMIN_PWD | kinit admin
 ## run record adds here after kinint for auth
 runuser -l root -c "ipa dnsrecord-add $INTERNAL_DOMAIN_NAME. '*' --a-ip-address=$GATEWAY_ROUTER_IP"
+runuser -l root -c "ipa dnsrecord-add $INTERNAL_DOMAIN_NAME. '$APP_INTERNAL_HOSTNAME' --a-ip-address=$INTERNAL_VIP"
 /usr/bin/ipa user-add --first=Firstname --last=Lastname ipauser
 SSH_KEY=`cat /root/.ssh/id_rsa.pub`
 /usr/bin/ipa user-mod ipauser --sshpubkey="$SSH_KEY"
