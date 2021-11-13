617616c3581067996cff48857c67bdb40e227be5
Chris Lyons <chris@lyonsgroup.family>
2021-11-13 12:08:31 -0500

    changing DNS from pfsense to identity

--- kolla.sh
+++ kolla.sh
@@ -911,7 +911,7 @@ if [[ $error_count -gt 0 ]]; then
                       -o /tmp/cf-deployment/operations/use-trusted-ca-cert-for-apps.yml \
                       -l /opt/stack/trusted-certs-cf.vars.yml \
                       -v system_domain=$INTERNAL_DOMAIN_NAME \
-                      -v auth_url=http://$INTERNAL_VIP_DNS:5000/v3 \
+                      -v auth_url=https://$EXTERNAL_VIP_DNS:5000/v3 \
                       -v openstack_project=cloudfoundry \
                       -v openstack_domain=default \
                       -v openstack_username=$OPENSTACK_CLOUDFOUNDRY_USERNAME \
