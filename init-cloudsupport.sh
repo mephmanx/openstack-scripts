d8712c5bfde3834e2b32b20eeae96d5be9e168b3
Chris Lyons <chris@lyonsgroup.family>
2021-11-16 10:44:16 -0500

    changing DNS from pfsense to identity

--- init-cloudsupport.sh
+++ init-cloudsupport.sh
@@ -105,9 +105,6 @@ chmod 700 *.sh
 
 runuser -l root -c  "cd /root/harbor; ./install.sh --with-notary --with-trivy --with-chartmuseum"
 
-### join to domain
-join_machine_to_domain $ADMIN_PWD
-
 telegram_notify $TELEGRAM_API $TELEGRAM_CHAT_ID "Cloudsupport VM ready for use"
 ##########################
 #remove so as to not run again
