93088ec7df81e583a8ac51216eef41ed9760e273
Chris Lyons <chris@lyonsgroup.family>
2021-11-23 19:56:35 -0400

    removing timezone variable

--- openstack-env-sample.sh
+++ openstack-env-sample.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ## domain name
+## generate internal domain name
 export INTERNAL_DOMAIN_NAME=<internal cloud domain name>;
 export EXTERNAL_DOMAIN_NAME=<domain name on external requests>
 
