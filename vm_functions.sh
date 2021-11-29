f76133e8f2e4cbe2f9b4f17e96358b9de1f643b0
Chris Lyons <chris@lyonsgroup.family>
2021-11-28 22:34:11 -0500

    removing harbor from domain for now

--- vm_functions.sh
+++ vm_functions.sh
@@ -499,3 +499,18 @@ function baseDN() {
   echo $BASE_DN
   ####
 }
+
+function generate_random_word() {
+  # Constants
+  X=0
+  ALL_NON_RANDOM_WORDS=/usr/share/dict/words
+
+  # total number of non-random words available
+  non_random_words=`cat $ALL_NON_RANDOM_WORDS | wc -l`
+
+  # while loop to generate random words
+  # number of random generated words depends on supplied argument
+
+  random_number=`od -N3 -An -i /dev/urandom | awk -v f=0 -v r="$non_random_words" '{printf "%i\n", f + r * $1 / 16777216}'`
+  sed `echo $random_number`"q;d" $ALL_NON_RANDOM_WORDS
+}
