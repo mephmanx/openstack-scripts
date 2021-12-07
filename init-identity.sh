ccafb3e53b4299f5aa41d8d58e776549e0cf0677
Chris Lyons <chris@lyonsgroup.family>
2021-12-07 10:03:30 -0500

    code cleanup

--- init-identity.sh
+++ init-identity.sh
@@ -97,6 +97,7 @@ runuser -l root -c "ipa-dns-install --auto-forwarders --auto-reverse --no-dnssec
 echo $ADMIN_PWD | kinit admin
 
 ## run record adds here after kinint for auth
+runuser -l root -c "ipa dnszone-mod $INTERNAL_DOMAIN_NAME. --allow-query=$NETWORK_PREFIX.0/24\;$LB_NETWORK.0/24\;$TROVE_NETWORK.0\;10.0.9.0/24"
 runuser -l root -c "ipa dnszone-mod $INTERNAL_DOMAIN_NAME. --allow-sync-ptr=TRUE"
 runuser -l root -c "ipa dnsrecord-add $INTERNAL_DOMAIN_NAME. '*' --a-ip-address=$LB_ROUTER_IP"
 runuser -l root -c "ipa dnsrecord-add $INTERNAL_DOMAIN_NAME. '$APP_INTERNAL_HOSTNAME' --a-ip-address=$INTERNAL_VIP"
