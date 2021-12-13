9864d7aeb05c406b2e06669566a04906e483fc92
Chris Lyons <chris@lyonsgroup.family>
2021-12-13 07:38:19 -0500

    adding vpn network

--- kolla.sh
+++ kolla.sh
@@ -1062,7 +1062,7 @@ cf update-quota default -i 2G -m 4G
 ### determine quota formula.  this is memory on compute server to be made available for cloudfoundry org.
 ## Remember, other VM's run on compute (amphora, DBaas, BOSH, docker/kube clusters, etc) so make sure to leave enough for them!
 memGB=$((mem / 1024 / 1024 - 32 * (CF_MEMORY_ALLOCATION_PCT / 100)))
-cf create-quota $INTERNAL_DOMAIN_NAME -i 8096M -m "$memGBG" -r 1000 -s 1000 -a 1000 --allow-paid-service-plans --reserved-route-ports $CF_TCP_PORT_COUNT
+cf create-quota $INTERNAL_DOMAIN_NAME -i 8096M -m "$memGB"G -r 1000 -s 1000 -a 1000 --allow-paid-service-plans --reserved-route-ports $CF_TCP_PORT_COUNT
 cf set-quota $INTERNAL_DOMAIN_NAME $INTERNAL_DOMAIN_NAME
 
 ### install security group
