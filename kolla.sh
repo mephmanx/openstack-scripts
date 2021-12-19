196a2878d06c63a95bced9913821246cef8f4c81
Chris Lyons <chris@lyonsgroup.family>
2021-12-19 05:00:34 -0500

    adding version tags

--- kolla.sh
+++ kolla.sh
@@ -873,7 +873,7 @@ openstack security group rule create --proto tcp --dst-port 1:65535 --remote-gro
 openstack security group rule create --proto tcp --dst-port 1:65535 --remote-group $(openstack security group show cf | grep id | head -n 1 | cut -d"|" -f3 | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }') bosh
 openstack security group rule create --proto tcp --dst-port 1:65535 --remote-group $(openstack security group show bosh | grep id | head -n 1 | cut -d"|" -f3 | awk '{ gsub(/^[ \t]+|[ \t]+$/, ""); print }') cf
 openstack server add security group bosh/0 bosh
-openstack server add security group cf bosh
+openstack server add security group cf bosh/0
 
 ## switch back to admin
 source /etc/kolla/admin-openrc.sh
