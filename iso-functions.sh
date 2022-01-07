67d4a02d8b5d722e06f025af22bb6a818618e87a
Chris Lyons <chris@lyonsgroup.family>
2022-01-06 22:48:41 -0500

    attempting to use install repo from hd

--- iso-functions.sh
+++ iso-functions.sh
@@ -107,7 +107,6 @@ function closeOutAndBuildKickstartAndISO {
     -e images/efiboot.img \
     -quiet \
     -allow-limited-size \
-    -iso-level 3 \
     -no-emul-boot -J -R -v -T -V 'CentOS-8-x86_64' .
 
   cd /var/tmp/
