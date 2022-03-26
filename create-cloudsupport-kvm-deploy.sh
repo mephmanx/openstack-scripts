d631409b6439dc790182393412dba68b0ac7875d
Chris Lyons <chris@lyonsgroup.family>
2022-03-26 16:34:28 -0400

    migrating to prebuilt isos

--- create-cloudsupport-kvm-deploy.sh
+++ create-cloudsupport-kvm-deploy.sh
@@ -17,7 +17,7 @@ fi
 DISK_COUNT=$(get_disk_count)
 if [[ $DISK_COUNT -lt 2 ]]; then
   size_avail=`df /VM-VOL-ALL | awk '{print $2}' | sed 1d`
-  DRIVE_SIZE=$(($((size_avail * 5/100)) / 1024 / 1024))
+  DRIVE_SIZE=$(($((size_avail * 10/100)) / 1024 / 1024))
 else
   size_avail=`df /VM-VOL-MISC | awk '{print $2}' | sed 1d`
   DRIVE_SIZE=$(($((size_avail * 45/100)) / 1024 / 1024))
