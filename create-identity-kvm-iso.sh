1a40c773cbecae89ab6ca171ed9515c398a3bf0b
Chris Lyons <chris@lyonsgroup.family>
2022-03-26 22:03:50 -0400

    migrating to prebuilt isos

--- create-identity-kvm-iso.sh
+++ create-identity-kvm-iso.sh
@@ -33,6 +33,7 @@ sed -i 's/{GENERATED_PWD}/'$(generate_random_pwd 31)'/g' ${kickstart_file}
 ###########################
 
 embed_files=('/tmp/id_rsa.crt'
+              '/tmp/id_rsa'
               '/tmp/id_rsa.pub'
               '/tmp/id_rsa.key'
               '/tmp/openstack-env.sh'
