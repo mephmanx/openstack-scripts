f843d75cf35d32f1f5d4f64615853c04b5556a5c
Chris Lyons <chris@lyonsgroup.family>
2022-03-30 18:44:35 -0400

    migrating to prebuilt isos

--- create-cloud-kvm-deploy.sh
+++ create-cloud-kvm-deploy.sh
@@ -5,7 +5,6 @@ exec 1>/root/cloud-install.log 2>&1 # send stdout and stderr from rc.local to a 
 #set -x
 
 source /tmp/vm_functions.sh
-source /tmp/iso-functions.sh
 source /tmp/vm-configurations.sh
 source /tmp/project_config.sh
 
