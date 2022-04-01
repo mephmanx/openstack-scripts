0b59db8cd35ce1f79a0afa08256875604e753202
Chris Lyons <chris@lyonsgroup.family>
2022-04-01 11:30:15 -0400

    migrating to prebuilt isos

--- pfsense-init.sh
+++ pfsense-init.sh
@@ -10,7 +10,7 @@ set -x                             # tell sh to display commands before executio
 
 IP_DATA=`ifconfig vtnet0 | grep inet | awk -F' ' '{ print $2 }' | head -2 | tail -1`
 DRIVE_SIZE=`geom disk list | grep Mediasize | sed 2d | awk '{ print $2 }'`
-sed -i 's/{CACHE_SIZE}/'$(($DRIVE_SIZE / 1024 / 1024 * 75/100))'/g' /conf/config.xml
+sed -i -e 's/{CACHE_SIZE}/'$(($DRIVE_SIZE / 1024 / 1024 * 75/100))'/g' /conf/config.xml
 telegram_notify  "PFSense initialization script beginning... \n\nCloud DMZ IP: $IP_DATA"
 
 ## preparing next reboot
