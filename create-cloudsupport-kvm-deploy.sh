9a1bc095fff45fd0b2805b5938543138128fae89
mephmanx <chris@lyonsgroup.family>
2022-04-05 19:37:21 -0400

    working through vm errors related to centos stream upgrade and offline install

--- create-cloudsupport-kvm-deploy.sh
+++ create-cloudsupport-kvm-deploy.sh
@@ -26,11 +26,9 @@ create_line+="--cdrom=/tmp/cloudsupport-iso.iso "
 create_line+="--network type=bridge,source=loc-static,model=virtio "
 create_line+="--os-variant=centos8 "
 create_line+="--graphics=vnc "
-create_line+="--autostart --wait -1; virsh reboot cloudsupport"
+create_line+="--autostart --wait -1; virsh reboot cloudsupport; rm -rf /tmp/cloudsupport-iso.iso"
 
 telegram_notify  "Creating cloudsupport vm"
 
 echo $create_line
 eval $create_line &
-
-rm -rf /tmp/cloudsupport-iso.iso
