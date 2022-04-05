041dac4024fd0892ea8ef2a16463cf8cb0c4b53d
mephmanx <chris@lyonsgroup.family>
2022-04-05 11:08:01 -0400

    working through vm errors related to centos stream upgrade and offline install

--- vm_functions.sh
+++ vm_functions.sh
@@ -641,9 +641,6 @@ function replace_values_in_root_isos() {
   ADMIN_PWD=$(generate_specific_pwd 31)
   GEN_PWD=$(generate_specific_pwd 15)
 
-  echo $ADMIN_PWD > /root/env_admin_pwd
-  echo $DIRECTORY_MGR_PWD > /tmp/directory_mgr_pwd
-
   ## Files can only be replaced if they can be considered to be on "one line"
   ##  ssh keys are on one line as would most binary files.  text files, scripts, etc have multiple lines and DO NOT work!
   iso_images="/tmp/*.iso"
