5ac819b4eddb741edf06659497e5d8c100db74e8
mephmanx <chris@lyonsgroup.family>
2022-04-07 12:29:34 -0400

    working through vm errors related to centos stream upgrade and offline install

--- create-kolla-kvm-deploy.sh
+++ create-kolla-kvm-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -rf /root/cloud-install.log
-exec 1>/root/cloud-install.log 2>&1 # send stdout and stderr from rc.local to a log file
+rm -rf /root/kolla-install.log
+exec 1>/root/kolla-install.log 2>&1 # send stdout and stderr from rc.local to a log file
 #set -x
 
 source /tmp/vm_functions.sh
