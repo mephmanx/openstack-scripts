abce8f7cd8b2c2ad447f56f5214f6caff8274cf0
mephmanx <chris@lyonsgroup.family>
2022-04-14 15:54:49 -0400

    changes to load all docker images from internal harbor server

--- openstack-env-sample.sh
+++ openstack-env-sample.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-### harbor and docker accounts
-export DOCKER_HUB_USER=<docker hub username>
-export DOCKER_HUB_PWD=<dokcer hub access token>
-##########
-
 ### telegram
 export TELEGRAM_API=<telegram api key>
 export TELEGRAM_CHAT_ID=<telegram chat id>
