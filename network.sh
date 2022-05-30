21bc73fe3e4295959866e94104e2fa05553adf8f
mephmanx <chris@lyonsgroup.family>
2022-05-30 14:49:16 -0400

    removing set -x

--- network.sh
+++ network.sh
@@ -11,7 +11,7 @@
 start() {
 
 exec 1>/tmp/openstack-install.log 2>&1 # send stdout and stderr from rc.local to a log file
-set -x                             # tell sh to display commands before execution
+#set -x                             # tell sh to display commands before execution
 
 ########## Add call to the beginning of all rc.local scripts as this wait guarantees network availability
 sleep 30
