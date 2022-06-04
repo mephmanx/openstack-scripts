40e98c8ca08d9b63e1e168fd5b380a2b7a9fafd5
chris <chris@lyonsgroup.family>
2022-06-04 14:35:58 -0400

    updating ca cert

--- create-pfsense-kvm-deploy.sh
+++ create-pfsense-kvm-deploy.sh
@@ -79,8 +79,6 @@ telegram_notify  "PFSense first reboot in progress, continuing to package instal
 runuser -l root -c  "rm -rf /tmp/usb"
 #####
 
-root_pw=$(generate_random_pwd 31)
-
 ### base64 files
 #HYPERVISOR_KEY=$(cat </tmp/pf_key | base64 | tr -d '\n\r')
 #HYPERVISOR_PUB_KEY=$(cat </tmp/pf_key.pub | base64 | tr -d '\n\r')
@@ -91,8 +89,6 @@ PFSENSE_INIT=$(cat </tmp/pfsense-init.sh | base64 | tr -d '\n\r')
 IP_OUT=$(cat </tmp/ip_out_update | base64 | tr -d '\n\r')
 
 ### pfsense prep
-#hypervisor_key_array=( $(echo $HYPERVISOR_KEY | fold -c250 ))
-#hypervisor_pub_array=( $(echo $HYPERVISOR_PUB_KEY | fold -c250 ))
 openstack_env_array=( $(echo $OPENSTACK_ENV | fold -c250 ))
 pf_functions_array=( $(echo $PF_FUNCTIONS | fold -c250 ))
 project_config_array=( $(echo $PROJECT_CONFIG | fold -c250 ))
@@ -101,29 +97,6 @@ ip_out_array=( $(echo $IP_OUT | fold -c250 ))
 
 (echo open 127.0.0.1 4568;
   sleep 120;
-#  echo "mkdir /root/.ssh";
-#  sleep 20;
-#  echo "touch /root/.ssh/id_rsa; touch /root/.ssh/id_rsa.pub; touch /root/.ssh/id_rsa.pub.enc; touch /root/.ssh/id_rsa.enc;";
-#  sleep 10;
-#  for element in "${hypervisor_pub_array[@]}"
-#  do
-#    echo "echo '$element' >> /root/.ssh/id_rsa.pub.enc";
-#    sleep 5;
-#  done
-#  for element in "${hypervisor_key_array[@]}"
-#  do
-#    echo "echo '$element' >> /root/.ssh/id_rsa.enc";
-#    sleep 5;
-#  done
-#  echo "openssl base64 -d -in /root/.ssh/id_rsa.pub.enc -out /root/.ssh/id_rsa.pub;";
-#  sleep 10;
-#  echo "openssl base64 -d -in /root/.ssh/id_rsa.enc -out /root/.ssh/id_rsa;";
-#  sleep 10;
-#  echo "chmod 600 /root/.ssh/*";
-#  sleep 10;
-#  echo "ssh-keyscan -H $LAN_CENTOS_IP >> ~/.ssh/known_hosts;";
-#  sleep 30;
-
   echo "touch /root/openstack-env.sh; touch /root/openstack-env.sh.enc;";
   sleep 10;
   for element in "${openstack_env_array[@]}"
