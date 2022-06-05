dc7254ab8e9a243aa64f7acabe69009c75314755
chris <chris@lyonsgroup.family>
2022-06-04 21:30:25 -0400

    updating ca cert

--- create-pfsense-kvm-deploy.sh
+++ create-pfsense-kvm-deploy.sh
@@ -39,6 +39,20 @@ create_line+="--autostart --wait 0"
 
 eval "$create_line"
 
+### base64 files
+OPENSTACK_ENV=$(cat </tmp/openstack-env.sh | base64 | tr -d '\n\r')
+PF_FUNCTIONS=$(cat </tmp/pf_functions.sh | base64 | tr -d '\n\r')
+PROJECT_CONFIG=$(cat </tmp/project_config.sh | base64 | tr -d '\n\r')
+PFSENSE_INIT=$(cat </tmp/pfsense-init.sh | base64 | tr -d '\n\r')
+IP_OUT=$(cat </tmp/ip_out_update | base64 | tr -d '\n\r')
+
+### pfsense prep
+openstack_env_array=( $(echo $OPENSTACK_ENV | fold -c250 ))
+pf_functions_array=( $(echo $PF_FUNCTIONS | fold -c250 ))
+project_config_array=( $(echo $PROJECT_CONFIG | fold -c250 ))
+pfsense_init_array=( $(echo $PFSENSE_INIT | fold -c250 ))
+ip_out_array=( $(echo $IP_OUT | fold -c250 ))
+
 sleep 30;
 (echo open 127.0.0.1 4568;
   sleep 60;
@@ -66,6 +80,51 @@ sleep 30;
   sleep 5;
   echo 'S';
   sleep 5;
+  echo "touch /mnt/root/openstack-env.sh; touch /mnt/root/openstack-env.sh.enc;";
+    sleep 10;
+    for element in "${openstack_env_array[@]}"
+    do
+      echo "echo '$element' >> /mnt/root/openstack-env.sh.enc";
+      sleep 5;
+    done
+    echo "openssl base64 -d -in /mnt/root/openstack-env.sh.enc -out /mnt/root/openstack-env.sh;";
+    sleep 10;
+
+    echo "touch /mnt/root/pf_functions.sh; touch /mnt/root/pf_functions.sh.enc;";
+    sleep 10;
+    for element in "${pf_functions_array[@]}"
+    do
+      echo "echo '$element' >> /mnt/root/pf_functions.sh.enc";
+      sleep 5;
+    done
+    echo "openssl base64 -d -in /mnt/root/pf_functions.sh.enc -out /mnt/root/pf_functions.sh;";
+    sleep 10;
+
+    echo "touch /mnt/root/project_config.sh; touch /mnt/root/project_config.sh.enc;";
+    sleep 10;
+    for element in "${project_config_array[@]}"
+    do
+      echo "echo '$element' >> /mnt/root/project_config.sh.enc";
+      sleep 5;
+    done
+    echo "openssl base64 -d -in /mnt/root/project_config.sh.enc -out /mnt/root/project_config.sh;";
+    sleep 10;
+
+    echo "touch /mnt/root/pfsense-init.sh; touch /mnt/root/pfsense-init.sh.enc;";
+    sleep 10;
+    for element in "${pfsense_init_array[@]}"
+    do
+      echo "echo '$element' >> /mnt/root/pfsense-init.sh.enc";
+      sleep 5;
+    done
+    echo "openssl base64 -d -in /mnt/root/pfsense-init.sh.enc -out /mnt/root/pfsense-init.sh;";
+    sleep 10;
+    echo "rm -rf /mnt/root/*.enc";
+    sleep 10;
+
+    echo "chmod 777 /mnt/root/*.sh"
+    sleep 10;
+    echo "reboot"
 ) | telnet
 
 ## remove install disk from pfsense
@@ -73,78 +132,11 @@ virsh detach-disk --domain pfsense /tmp/pfSense-CE-memstick-ADI.img --persistent
 virsh reboot pfsense
 
 sleep 120;
-telegram_notify  "PFSense first reboot in progress, continuing to package install...."
-
 ### cleanup
 runuser -l root -c  "rm -rf /tmp/usb"
 #####
 
-### base64 files
-#HYPERVISOR_KEY=$(cat </tmp/pf_key | base64 | tr -d '\n\r')
-#HYPERVISOR_PUB_KEY=$(cat </tmp/pf_key.pub | base64 | tr -d '\n\r')
-OPENSTACK_ENV=$(cat </tmp/openstack-env.sh | base64 | tr -d '\n\r')
-PF_FUNCTIONS=$(cat </tmp/pf_functions.sh | base64 | tr -d '\n\r')
-PROJECT_CONFIG=$(cat </tmp/project_config.sh | base64 | tr -d '\n\r')
-PFSENSE_INIT=$(cat </tmp/pfsense-init.sh | base64 | tr -d '\n\r')
-IP_OUT=$(cat </tmp/ip_out_update | base64 | tr -d '\n\r')
-
-### pfsense prep
-openstack_env_array=( $(echo $OPENSTACK_ENV | fold -c250 ))
-pf_functions_array=( $(echo $PF_FUNCTIONS | fold -c250 ))
-project_config_array=( $(echo $PROJECT_CONFIG | fold -c250 ))
-pfsense_init_array=( $(echo $PFSENSE_INIT | fold -c250 ))
-ip_out_array=( $(echo $IP_OUT | fold -c250 ))
-
-(echo open 127.0.0.1 4568;
-  sleep 120;
-  echo "touch /root/openstack-env.sh; touch /root/openstack-env.sh.enc;";
-  sleep 10;
-  for element in "${openstack_env_array[@]}"
-  do
-    echo "echo '$element' >> /root/openstack-env.sh.enc";
-    sleep 5;
-  done
-  echo "openssl base64 -d -in /root/openstack-env.sh.enc -out /root/openstack-env.sh;";
-  sleep 10;
-
-  echo "touch /root/pf_functions.sh; touch /root/pf_functions.sh.enc;";
-  sleep 10;
-  for element in "${pf_functions_array[@]}"
-  do
-    echo "echo '$element' >> /root/pf_functions.sh.enc";
-    sleep 5;
-  done
-  echo "openssl base64 -d -in /root/pf_functions.sh.enc -out /root/pf_functions.sh;";
-  sleep 10;
-
-  echo "touch /root/project_config.sh; touch /root/project_config.sh.enc;";
-  sleep 10;
-  for element in "${project_config_array[@]}"
-  do
-    echo "echo '$element' >> /root/project_config.sh.enc";
-    sleep 5;
-  done
-  echo "openssl base64 -d -in /root/project_config.sh.enc -out /root/project_config.sh;";
-  sleep 10;
-
-  echo "touch /root/pfsense-init.sh; touch /root/pfsense-init.sh.enc;";
-  sleep 10;
-  for element in "${pfsense_init_array[@]}"
-  do
-    echo "echo '$element' >> /root/pfsense-init.sh.enc";
-    sleep 5;
-  done
-  echo "openssl base64 -d -in /root/pfsense-init.sh.enc -out /root/pfsense-init.sh;";
-  sleep 10;
-  echo "rm -rf /root/*.enc";
-  sleep 10;
-
-  echo "chmod 777 /root/*.sh"
-  sleep 10;
-  echo "reboot"
-) | telnet
-
-telegram_notify  "PFSense rebooting after package install, pfsense-init script should begin after reboot."
+telegram_notify  "PFSense reboot, pfsense-init script should begin after reboot."
 
 rm -rf /tmp/pfSense-CE-memstick-ADI.img
 
