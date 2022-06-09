fd3666652c4663efac9d63d8c18dc98b72b2fc11
mephmanx <chris@lyonsgroup.family>
2022-06-09 14:31:50 -0400

    moving bond create to install

--- vm_functions.sh
+++ vm_functions.sh
@@ -428,6 +428,13 @@ EOF
 
   ### if possible, restart selinux
   runuser -l root -c "sed -i 's/\(SELINUX\=\).*/\1enabled/' /etc/selinux/config"
+
+cat << EOF >> /etc/ssh/sshd_config
+AuthorizedKeysCommand /usr/local/bin/sss_ssh_authorizedkeys
+AuthorizedKeysCommandUser nobody
+PubkeyAuthentication yes
+EOF
+
 }
 
 function baseDN() {
