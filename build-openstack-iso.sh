94e45e115d04117d645f3e4b2e21c28579c623eb
mephmanx <chris@lyonsgroup.family>
2022-06-08 20:41:56 -0400

    moving bond create to install

--- build-openstack-iso.sh
+++ build-openstack-iso.sh
@@ -188,6 +188,7 @@ fi
 embed_files=('/tmp/openstack-env.sh'
               '/tmp/project_config.sh'
               '/tmp/id_rsa'
+              '/tmp/id_rsa.crt'
               '/tmp/id_rsa.pub'
               '/tmp/wildcard.crt'
               '/tmp/wildcard.key'
