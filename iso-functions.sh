be97a9d09fba7c8729ae3fea9cf44a0c2179f480
mephmanx <chris@lyonsgroup.family>
2022-06-10 12:32:50 -0400

    adding local repo for docker

--- iso-functions.sh
+++ iso-functions.sh
@@ -146,7 +146,7 @@ function buildAndPushOpenstackSetupISO {
                 '/tmp/id_rsa'
                 '/tmp/id_rsa.pub'
                 '/tmp/storage_hosts'
-                "/tmp/docker-repo.zip"
+                "/tmp/docker-repo.tar"
                 "/tmp/amphora-x64-haproxy-$AMPHORA_VERSION.qcow2"
                 '/tmp/openstack-scripts/kolla.sh'
                 "/tmp/harbor/$OPENSTACK_VERSION/globals.yml"
