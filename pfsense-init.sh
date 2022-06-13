d190c4ef8a625b22591cfa2bdc04bf44acc941cb
mephmanx <chris@lyonsgroup.family>
2022-06-12 22:04:23 -0400

    using pfsense install img to copy initial files

--- pfsense-init.sh
+++ pfsense-init.sh
@@ -6,7 +6,7 @@
 . /root/pf_functions.sh
 
 exec 1>/root/init-install.log 2>&1 # send stdout and stderr from rc.local to a log file
-set -x                             # tell sh to display commands before execution
+#set -x                             # tell sh to display commands before execution
 
 IP_DATA=$(ifconfig vtnet0 | grep inet | awk -F' ' '{ print $2 }' | head -2 | tail -1)
 telegram_notify  "PFSense initialization script beginning... \n\nCloud DMZ IP: $IP_DATA"
