524be733a78ef962c074eedf7639cc4373a18a21
mephmanx <chris@lyonsgroup.family>
2022-06-12 21:53:17 -0400

    using pfsense install img to copy initial files

--- pfsense-init.sh
+++ pfsense-init.sh
@@ -20,22 +20,5 @@ install_pkg "pfsense-pkg-cron"
 install_pkg "pfsense-pkg-Telegraf"
 install_pkg "qemu-guest-agent"
 
-## the pfsense method for changing config via cli is f*ed up:
-##  change all backup files, delete primary file, and let system "restore" a changed backup file...makes a lot of sense, huh?
-## DO NOT USE $() notation below!!!   IT WILL NOT WORK ON PFSENSE!!
-# shellcheck disable=SC2006
-DRIVE_KB=`geom disk list | grep Mediasize | sed 2d | awk '{ print $2 }'`
-DRIVE_SIZE=$((DRIVE_KB / 1024 / 1024 * 75/100))
-echo "Setting cache size to $DRIVE_SIZE"
-
-files="/cf/conf/backup/*"
-for file in $files; do
-  echo "Changing contents of file $file"
-  perl -pi.back -e "s/{CACHE_SIZE}/$DRIVE_SIZE/g;" "$file"
-done
-
-rm -rf /cf/conf/config.xml
 rm -rf /root/pfsense-init.sh
-## try restarting squid service instead of full reboot
-telegram_notify  "PFSense init: init complete! removing script and rebooting.."
-reboot
+telegram_notify  "PFSense init: init complete!"
