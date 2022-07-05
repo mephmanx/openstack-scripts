52acf51d9671cad7d5a335514ae53767038870f3
Chris Lyons <chris@lyonsgroup.family>
2022-07-05 16:47:23 -0400

    new device names are ok on hypervisor

--- build-openstack-iso.sh
+++ build-openstack-iso.sh
@@ -178,8 +178,6 @@ do
 done
 ####
 
-yum install -y centos-release-openstack-"${OPENSTACK_VERSION}"
-yum install -y openstack-kolla
 if [ ! -f "/tmp/harbor/$OPENSTACK_VERSION/centos-binary-base-${OPENSTACK_VERSION}.tar" ] && [ ! -f "/tmp/harbor/$OPENSTACK_VERSION/kolla_${OPENSTACK_VERSION}_rpm_repo.tar.gz" ]; then
     for i in $(docker images |grep rpm_repo|awk '{print $3}');do docker rmi "$i";done
     for i in $(docker images |grep kolla|awk '{print $3}');do docker rmi "$i";done
