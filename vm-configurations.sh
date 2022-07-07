45d1de37dc627ea568792b8e6f224eddc51fe25a
Chris Lyons <chris@lyonsgroup.family>
2022-07-06 20:31:18 -0400

    refactoring

--- vm-configurations.sh
+++ vm-configurations.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 . ./project_config.sh
-. ./vm_functions.sh
+
+function parse_json() {
+  echo "$1" |
+    sed -e 's/[{}]/''/g' |
+    sed -e 's/", "/'\",\"'/g' |
+    sed -e 's/" ,"/'\",\"'/g' |
+    sed -e 's/" , "/'\",\"'/g' |
+    sed -e 's/","/'\"---SEPERATOR---\"'/g' |
+    awk -F=':' -v RS='---SEPERATOR---' "\$1~/\"$2\"/ {print}" |
+    sed -e "s/\"$2\"://" |
+    tr -d "\n\t" |
+    sed -e 's/\\"/"/g' |
+    sed -e 's/\\\\/\\/g' |
+    sed -e 's/^[ \t]*//g' |
+    sed -e 's/^"//' -e 's/"$//'
+}
 
 function getVMCount {
   option="${1}"
