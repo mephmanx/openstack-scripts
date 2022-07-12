84cb29c6ec7d2eb05a4238ea4b7923a79358b866
Chris Lyons <chris@lyonsgroup.family>
2022-07-12 18:26:45 -0400

    removing unneeded functions

--- init-identity.sh
+++ init-identity.sh
@@ -305,11 +305,11 @@ mkdir /tmp/empty_dir
 
 openssl genrsa -out /tmp/sub-ca.key 4096
 
-file_length_pk=$(wc -c "/tmp/id_rsa" | awk -F' ' '{ print $1 }')
+file_length_pk=$(wc -c "/tmp/sub-ca.key" | awk -F' ' '{ print $1 }')
 file_length_old=3247
 while [ "$file_length_pk" != "$file_length_old" ]; do
-  runuser -l root -c  "openssl genrsa -out /tmp/id_rsa 4096"
-  file_length_pk=$(wc -c "/tmp/id_rsa" | awk -F' ' '{ print $1 }')
+  runuser -l root -c  "openssl genrsa -out /tmp/sub-ca.key 4096"
+  file_length_pk=$(wc -c "/tmp/sub-ca.key" | awk -F' ' '{ print $1 }')
 done
 
 openssl rsa -in /tmp/sub-ca.key -out /tmp/empty_dir/sub-ca.key
