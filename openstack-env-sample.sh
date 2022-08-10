08c419656980d851d3e9a1efee3ae7fc963a2963
chris <chris@lyonsgroup.family>
2022-08-10 07:56:53 -0400

    refactoring OS cloud vm create

--- openstack-env-sample.sh
+++ openstack-env-sample.sh
@@ -4,11 +4,3 @@
 export TELEGRAM_API=<telegram api key>
 export TELEGRAM_CHAT_ID=<telegram chat id>
 #########
-
-#### Snort key
-export OINKMASTER=<oinkmaster key>
-#########
-
-###### Maxmind key
-export MAXMIND_KEY=<maxmind key>
-##########
