898cb7884269ebdbe2365076820259374287c961
chris <chris@lyonsgroup.family>
2022-08-11 17:48:34 -0400

    refactoring OS cloud vm create

--- iso-functions.sh
+++ iso-functions.sh
@@ -100,6 +100,7 @@ function buildAndPushVMTypeISO {
   #####################################
   embed_files=('/tmp/openstack-scripts/vm_functions.sh'
                 '/tmp/openstack-scripts/project_config.sh'
+                '/tmp/openstack-setup/openstack-env.sh'
                 '/tmp/docker-repo.tar')
 
   printf -v embed_files_string '%s ' "${embed_files[@]}"
