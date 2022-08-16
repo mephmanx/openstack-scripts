5e1fe464dd06ec6c4ef370b608bc3f6a459b9e29
chris <chris@lyonsgroup.family>
2022-08-16 12:39:51 -0400

    cleaning up tmp directory

--- init-identity.sh
+++ init-identity.sh
@@ -69,7 +69,7 @@ runuser -l root -c "ipa dnsrecord-add $INTERNAL_DOMAIN_NAME. _ntp._udp --srv-pri
 mkdir -p /opt/stack/.ssh
 /usr/bin/ipa user-add --first=Stack --last=account stack --random --shell=/bin/bash --homedir=/opt/stack
 ssh-keygen -t rsa -f /opt/stack/.ssh/id_rsa -q -P ""
-user-mod stack --sshpubkey="$(cat /opt/stack/.ssh/id_rsa.pub)"
+/usr/bin/ipa user-mod stack --sshpubkey="$(cat /opt/stack/.ssh/id_rsa.pub)"
 
 
 #Add sudo rules
