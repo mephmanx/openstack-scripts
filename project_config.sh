76a3cf4693be065a8daae1b65997162120427e3b
chris <chris@lyonsgroup.family>
2022-08-19 09:14:53 -0400

    cleaning up tmp directory

--- project_config.sh
+++ project_config.sh
@@ -102,8 +102,8 @@ export CF_MEMORY_ALLOCATION_PCT=80
 export CF_DISK_ALLOCATION=80
 ### this must be in FreeBSD compatible array format or will break the pfsense init!
 ## stemcells to cache, will be pulled from https://bosh.io/d/stemcells/bosh-openstack-kvm-ubuntu-$stemcell-go_agent
-export CF_STEMCELLS="ubuntu-bionic ubuntu-xenial ubuntu-trusty"
-export BOSH_STEMCELL="ubuntu-bionic"
+export CF_STEMCELLS="ubuntu-jammy ubuntu-bionic ubuntu-xenial ubuntu-trusty"
+export BOSH_STEMCELL="ubuntu-jammy"
 ## openstack vm counts
 ### these counts can be adjusted if larger than 1 server
 ### below counts are based on single server
