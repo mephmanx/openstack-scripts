cf44f7d007e2cc502013a391f555017d02543e23
chris <chris@lyonsgroup.family>
2022-09-10 19:01:52 -0400

    updating filenames to match container names

--- vm_functions.sh
+++ vm_functions.sh
@@ -20,13 +20,6 @@ function grow_fs() {
 }
 
 function telegram_notify() {
-#  token=$TELEGRAM_API
-#  chat_id=$TELEGRAM_CHAT_ID
-#  msg_text=$1
-#  curl -X POST  \
-#        -H 'Content-Type: application/json' -d "{\"chat_id\": \"$chat_id\", \"text\":\"$msg_text\", \"disable_notification\":false}"  \
-#        -s \
-#        https://api.telegram.org/bot"$token"/sendMessage > /dev/null
   echo "$@" >&2
   SCRIPT_NAME="$(hostname -s)-$(basename "$0")"
   logger -p news.alert -t "$SCRIPT_NAME" "$@"
