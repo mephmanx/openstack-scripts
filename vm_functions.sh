2335055c39909234faa597aab5e46ed58cf95757
chris <chris@lyonsgroup.family>
2022-09-19 14:35:28 -0400

    sending log entries to /var/log

--- vm_functions.sh
+++ vm_functions.sh
@@ -353,7 +353,7 @@ function install_packages_hypervisor() {
 \$template myedit,"%msg%\n"
 
 \$template remote-incoming-logs, "/var/log/%HOSTNAME%/%PROGRAMNAME%.log"
-*.* remote-incoming-logs
+*.* -?remote-incoming-logs
 EOT
   cat /etc/rsyslog-end.conf >> /etc/rsyslog-start.conf
   rm -rf /etc/rsyslog.conf
