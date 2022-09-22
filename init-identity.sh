c3b6e625814ac7e9cded6abd6434debcfb9b08a2
chris <chris@lyonsgroup.family>
2022-09-22 06:17:19 -0400

    sending log entries to /var/log

--- init-identity.sh
+++ init-identity.sh
@@ -55,6 +55,7 @@ runuser -l root -c "ipa dnsrecord-add $INTERNAL_DOMAIN_NAME. '*' --a-ip-address=
 runuser -l root -c "ipa dnsrecord-add $INTERNAL_DOMAIN_NAME. '$APP_INTERNAL_HOSTNAME' --a-ip-address=$INTERNAL_VIP"
 runuser -l root -c "ipa dnsrecord-add $INTERNAL_DOMAIN_NAME. '$APP_EXTERNAL_HOSTNAME' --a-ip-address=$EXTERNAL_VIP"
 runuser -l root -c "ipa dnsrecord-add $INTERNAL_DOMAIN_NAME. '$SUPPORT_HOST' --a-ip-address=$SUPPORT_VIP"
+runuser -l root -c "ipa dnsrecord-add $INTERNAL_DOMAIN_NAME. '$IDENTITY_HOST' --a-ip-address=$IDENTITY_VIP"
 runuser -l root -c "ipa dnsrecord-add $INTERNAL_DOMAIN_NAME. _ntp._udp --srv-priority=0 --srv-weight=100 --srv-port=123 --srv-target=$EDGE_ROUTER_NAME.$INTERNAL_DOMAIN_NAME."
 
 #### groups
